9beb15608ce87353e14867942ab5d2a4c73e2f8e
RaffDev31 <raffli4000@hotmail.com>
2024-10-28 14:40:53 +0700

    feat(routes/web): add new route for forgot password

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
+// Redirect the root to the login page
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('login');
 });
+
+
+Route::get('login', [AuthController::class, 'index'])->name('login');
+Route::get('forgot-password', [AuthController::class, 'showForgotPasswordForm'])->name('forgot-password');
+Route::post('forgot-password', [AuthController::class, 'sendResetLink'])->name('forgot-password-send');
+Route::get('reset-password/{token}', [AuthController::class, 'showResetPasswordForm'])->name('reset-password');
+Route::post('reset-password', [AuthController::class, 'resetPassword'])->name('reset-password-send');
