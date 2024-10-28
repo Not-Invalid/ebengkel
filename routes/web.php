63890387af03f595728826b00435d558377e4a66
RaffDev31 <raffli4000@hotmail.com>
2024-10-28 16:07:51 +0700

    feat(routes): add registration and login submission routes

--- routes/web.php
+++ routes/web.php
@@ -9,7 +9,11 @@ Route::get('/', function () {
 });
 
 
-Route::get('login', [AuthController::class, 'index'])->name('login');
+Route::get('login', [AuthController::class, 'showLoginForm'])->name('login');
+Route::post('login', [AuthController::class, 'login'])->name('login-send');
+Route::get('register', [AuthController::class, 'showRegisterForm'])->name('register');
+Route::post('register', [AuthController::class, 'register'])->name('register-send');
+
 Route::get('forgot-password', [AuthController::class, 'showForgotPasswordForm'])->name('forgot-password');
 Route::post('forgot-password', [AuthController::class, 'sendResetLink'])->name('forgot-password-send');
 Route::get('reset-password/{token}', [AuthController::class, 'showResetPasswordForm'])->name('reset-password');
