181574fd576bde697e273e2e13292789270f4f53
RaffDev31 <raffli4000@hotmail.com>
2024-10-29 16:22:42 +0700

    feat(auth): enhance login page functionality and styling

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title> Ebengkel | Login</title> 
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="{{ asset('assets/css/auth.css') }}">
     <link href='https://unpkg.com/boxicons@2.1.1/css/boxicons.min.css' rel='stylesheet'>
 
@@ -11,7 +12,14 @@
 <body>
   <div class="wrapper">
     <h2>Login</h2>
+    @if (session('status'))
+        <div class="alert alert-success">
+            {{ session('status') }}
+        </div>
+    @endif
+
     <form action="{{ route('login-send') }}" method="POST">
+      @csrf
       <div class="input-box">
         <input type="text" name="email" placeholder="Enter your email" required>
       </div>
@@ -24,11 +32,18 @@
       <div class="forgot">
         <h3><a href="{{ route('forgot-password') }}">Forgot Password</a></h3>
       </div>
+      @if ($errors->any())
+          <div class="fs-6 text-danger">
+              @foreach ($errors->all() as $error)
+                  <p>{{ $error }}</p>
+              @endforeach
+          </div>
+      @endif
       <div class="input-box button">
-        <input type="Submit" value="Register Now">
+        <input type="Submit" value="Login">
       </div>
       <div class="text">
-        <h3>Don't have an account? <a href="{{ route('register') }}">Login</a></h3>
+        <h3>Don't have an account? <a href="{{ route('register') }}">Register now</a></h3>
       </div>
     </form>
   </div>
