acb729a7cd04a74093ca3161fce7a9ef5a735392
RaffDev31 <raffli4000@hotmail.com>
2024-10-31 16:11:42 +0700

    feat(footer): add new route links for support center

--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -26,7 +26,7 @@
           <div class="footer-link">
             <h6 class="footer-title">Help & Support</h6>
             <ul>
-              <li><a href="#">Support Center</a></li>
+              <li><a href="{{ route('support-center') }}">Support Center</a></li>
               <li><a href="{{ route('faqs') }}">FAQs</a></li>
               <li><a href="#">Terms & Conditions</a></li>
             </ul>
