7da87d857ddc32e6609fb3cc152260e0b306b4c4
dinoarr <dinoananda.restyanto@gmail.com>
2024-11-01 16:35:47 +0700

    feat:add title yield

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,64 +2,65 @@
 <html lang="en">
 
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <meta name="description" content="Bengkel Service, Spare Part & Smart Tools.">
-  <link rel="shortcut icon" href="{{ asset('assets/images/logo/icon.png') }}" type="image/x-icon" />
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="description" content="Bengkel Service, Spare Part & Smart Tools.">
+    <link rel="shortcut icon" href="{{ asset('assets/images/logo/icon.png') }}" type="image/x-icon" />
+    <title>@yield('title')</title>
 
-  {{-- Bootstrap CSS --}}
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css">
+    {{-- Bootstrap CSS --}}
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css">
 
-  {{-- Toastr CSS --}}
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css">
+    {{-- Toastr CSS --}}
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css">
 
-  {{-- Boxicons --}}
-  <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    {{-- Boxicons --}}
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
 
-  {{-- Custom styles --}}
-  <link rel="stylesheet" href="{{ asset('assets/css/partials.css') }}">
-  <link rel="stylesheet" href="{{ asset('assets/css/custom.css') }}">
+    {{-- Custom styles --}}
+    <link rel="stylesheet" href="{{ asset('assets/css/partials.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/custom.css') }}">
 
-  {{-- Poppins font --}}
-  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins:400,500,600,700,800">
-  @stack('css')
+    {{-- Poppins font --}}
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins:400,500,600,700,800">
+    @stack('css')
 </head>
 
 <body class="@yield('body-class')">
-  {{-- Loader --}}
-  <div id="loader">
-    <div id="center">
-      <center>
-        <img src="{{ asset('assets/images/components/loader.gif') }}" style="width: 170px;">
-        <p id="message" class="text-info" style="display:none;"></p>
-      </center>
+    {{-- Loader --}}
+    <div id="loader">
+        <div id="center">
+            <center>
+                <img src="{{ asset('assets/images/components/loader.gif') }}" style="width: 170px;">
+                <p id="message" class="text-info" style="display:none;"></p>
+            </center>
+        </div>
     </div>
-  </div>
 
-  {{-- JavaScript --}}
-  @yield('script')
+    {{-- JavaScript --}}
+    @yield('script')
 
-  {{-- Header --}}
-  <header>
-    @include('layouts.partials.navbar')
-  </header>
+    {{-- Header --}}
+    <header>
+        @include('layouts.partials.navbar')
+    </header>
 
-  {{-- Main content --}}
-  <main>
-    @yield('content')
-  </main>
+    {{-- Main content --}}
+    <main>
+        @yield('content')
+    </main>
 
-  {{-- Footer --}}
-  @include('layouts.partials.footer')
+    {{-- Footer --}}
+    @include('layouts.partials.footer')
 
-  {{-- Bootstrap JS --}}
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"></script>
-  <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/2.10.2/umd/popper.min.js"></script>
+    {{-- Bootstrap JS --}}
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/2.10.2/umd/popper.min.js"></script>
 
-  {{-- Toastr JS --}}
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
+    {{-- Toastr JS --}}
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
 
     <script>
         // Toastr configuration
@@ -80,41 +81,43 @@
             toastr.error("{{ session('status_error') }}");
         @endif
     </script>
-  {{-- Loader script --}}
-  <script>
-    function show(value) {
-      document.getElementById('loader').style.display = value ? 'block' : 'none';
-    }
-    function loadPage(URL) {
-      show(true);
-      location = URL;
-    }
-    function newTab(URL) {
-      window.open(URL, '_blank');
-    }
-    setTimeout(function() {
-      show(false);
-    }, 2000);
-  </script>
+    {{-- Loader script --}}
+    <script>
+        function show(value) {
+            document.getElementById('loader').style.display = value ? 'block' : 'none';
+        }
 
-  {{-- Currency format --}}
-  <script type="text/javascript">
-    function formatRupiah(angka) {
-      var number_string = angka.toString(),
-        split = number_string.split(','),
-        sisa = split[0].length % 3,
-        rupiah = split[0].substr(0, sisa),
-        ribuan = split[0].substr(sisa).match(/\d{3}/gi);
+        function loadPage(URL) {
+            show(true);
+            location = URL;
+        }
 
-      if (ribuan) {
-        separator = sisa ? ',' : '';
-        rupiah += separator + ribuan.join(',');
-      }
+        function newTab(URL) {
+            window.open(URL, '_blank');
+        }
+        setTimeout(function() {
+            show(false);
+        }, 2000);
+    </script>
 
-      rupiah = split[1] != undefined ? rupiah + ',' + split[1] : rupiah;
-      return rupiah;
-    }
-  </script>
+    {{-- Currency format --}}
+    <script type="text/javascript">
+        function formatRupiah(angka) {
+            var number_string = angka.toString(),
+                split = number_string.split(','),
+                sisa = split[0].length % 3,
+                rupiah = split[0].substr(0, sisa),
+                ribuan = split[0].substr(sisa).match(/\d{3}/gi);
+
+            if (ribuan) {
+                separator = sisa ? ',' : '';
+                rupiah += separator + ribuan.join(',');
+            }
+
+            rupiah = split[1] != undefined ? rupiah + ',' + split[1] : rupiah;
+            return rupiah;
+        }
+    </script>
 </body>
 
 </html>
