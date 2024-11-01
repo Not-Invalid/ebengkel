9779ad3cd8d8091739236bc728e8cd55e7bb167d
RaffDev31 <raffli4000@hotmail.com>
2024-11-01 15:26:36 +0700

    style: update category layout for support center

--- resources/views/pages/support_center.blade.php
+++ resources/views/pages/support_center.blade.php
@@ -12,61 +12,61 @@
       <div class="row mt-6">
         <div class="col-12 mb-4">
           <span class="badge bg-pastel-primary text-primary text-uppercase-bold-sm">
-            Topic categories
+            Categories
           </span>
         </div>
 
         <!-- Category -->
-        <div class="col-md-3 mb-4">
-          <a href="#" class="card align-items-center text-decoration-none border-1 hover-lift-light py-4 category">
+        <div class="col-12 col-sm-6 col-md-4 col-lg-3 mb-4">
+          <a href="#" class="card justify-content-center align-items-center shadow text-decoration-none border-1 hover-lift-light py-4 category">
             <span class="icon-circle icon-circle-lg bg-pastel-primary text-primary">
-              <i class='bx bx-info-circle fs-2'></i>
+              <i class='bx bxs-info-circle fs-2'></i>
             </span>
-            <span class="text-dark mt-3">
+            <span class="text-dark text-center mt-3">
               eBengkelku
             </span>
           </a>
         </div>
 
         <!-- Category -->
-        <div class="col-md-3 mb-4">
-          <a href="#" class="card align-items-center text-decoration-none border-1 hover-lift-light py-4 category">
+        <div class="col-12 col-sm-6 col-md-4 col-lg-3 mb-4">
+          <a href="#" class="card justify-content-center align-items-center shadow text-decoration-none border-1 hover-lift-light py-4 category">
             <span class="icon-circle icon-circle-lg bg-pastel-primary text-primary">
               <i class='bx bxs-lock fs-2'></i>
             </span>
-            <span class="text-dark mt-3">
+            <span class="text-dark text-center mt-3">
               Informasi Hukum dan Privasi
             </span>
           </a>
         </div>
 
         <!-- Category -->
-        <div class="col-md-3 mb-4">
-          <a href="#" class="card align-items-center text-decoration-none border-1 hover-lift-light py-4 category">
+        <div class="col-12 col-sm-6 col-md-4 col-lg-3 mb-4">
+          <a href="#" class="card justify-content-center align-items-center shadow text-decoration-none border-1 hover-lift-light py-4 category">
             <span class="icon-circle icon-circle-lg bg-pastel-primary text-primary">
               <i class='bx bxs-user-pin fs-2'></i>
             </span>
-            <span class="text-dark mt-3">
+            <span class="text-dark text-center mt-3">
               Pengaturan Akun dan Profile
             </span>
           </a>
         </div>
 
         <!-- Category -->
-        <div class="col-md-3 mb-4">
-          <a href="#" class="card align-items-center text-decoration-none border-1 hover-lift-light py-4 category">
+        <div class="col-12 col-sm-6 col-md-4 col-lg-3 mb-4">
+          <a href="#" class="card justify-content-center align-items-center shadow text-decoration-none border-1 hover-lift-light py-4 category">
             <span class="icon-circle icon-circle-lg bg-pastel-primary text-primary">
               <i class='bx bxs-shield fs-2'></i>
             </span>
-            <span class="text-dark mt-3">
+            <span class="text-dark text-center mt-3">
               Panduan Keamanan
             </span>
           </a>
         </div>
 
         <!-- Category -->
-        <div class="col-md-3 mb-4">
-          <a href="#" class="card align-items-center text-decoration-none border-1 hover-lift-light py-4 category">
+        <div class="col-12 col-sm-6 col-md-4 col-lg-3 mb-4">
+          <a href="#" class="card justify-content-center align-items-center shadow text-decoration-none border-1 hover-lift-light py-4 category">
             <span class="icon-circle icon-circle-lg bg-pastel-primary text-primary">
               <i class='bx bxs-wallet-alt fs-2'></i>
             </span>
@@ -79,3 +79,4 @@
       </div>
     </div>
 </section>
+@stop
