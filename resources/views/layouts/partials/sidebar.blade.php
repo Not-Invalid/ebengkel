60717871dac4513ab8e3a5d3940530c7650e7e4c
RaffDev31 <raffli4000@hotmail.com>
2024-11-01 15:31:01 +0700

    refactor: remove unused Font Awesome stylesheet link and clean up comments

--- resources/views/layouts/partials/sidebar.blade.php
+++ resources/views/layouts/partials/sidebar.blade.php
@@ -14,7 +14,6 @@
 
     {{-- Boxicons --}}
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" />
 
     {{-- Custom styles --}}
     <link rel="stylesheet" href="{{ asset('assets/css/sidebar_profile.css') }}">
