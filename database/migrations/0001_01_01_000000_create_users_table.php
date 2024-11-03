a6c985aec458998a5213e0d8500b27131879d7ca
RaffDev31 <raffli4000@hotmail.com>
2024-11-03 20:31:36 +0700

    feat(migrations): add new columns in users table

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -16,7 +16,9 @@ return new class extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
+            $table->enum('role', ['Administrator', 'User']);
             $table->string('password');
+            $table->text('foto_profile')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
