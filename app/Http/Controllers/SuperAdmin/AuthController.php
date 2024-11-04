cb7b4bb71dde481ec82a2aeee7fd25a13208a5b6
RaffDev31 <raffli4000@hotmail.com>
2024-11-04 15:06:20 +0700

    feat(superadmin/auth): add new method and logic authentication for superadmin

--- app/Http/Controllers/SuperAdmin/AuthController.php
+++ app/Http/Controllers/SuperAdmin/AuthController.php
@@ -4,8 +4,31 @@ namespace App\Http\Controllers\SuperAdmin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
 
 class AuthController extends Controller
 {
-    //
+    public function showLogin()
+    {
+        return view('superadmin.auth.login');
+    }
+
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        if (Auth::attempt(['email' => $request->email, 'password' => $request->password])) {
+            $request->session()->regenerate();
+            return redirect()->intended('/superadmin');
+        }
+
+        return back()->withErrors([
+            'email' => 'The provided credentials do not match our records.',
+        ]);
+    }
 }
