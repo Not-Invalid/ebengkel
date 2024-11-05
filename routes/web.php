bcd2e0802c7f6b46401273ef5460950f18c2b599
RaffDev31 <raffli4000@hotmail.com>
2024-11-05 15:31:14 +0700

    feat(routes): refactor and organize authentication routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\AuthController;
+use App\Http\Controllers\AuthController as PelangganAuthController;
 use App\Http\Controllers\EventController;
 use App\Http\Controllers\MessageController;
 use App\Http\Controllers\PageController;
@@ -15,16 +15,27 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [PageController::class, 'index'])->name('home');
 
 // Auth routes
-Route::get('login', [AuthController::class, 'showLoginForm'])->name('login');
-Route::post('login', [AuthController::class, 'login'])->name('login-send');
-Route::get('register', [AuthController::class, 'showRegisterForm'])->name('register');
-Route::post('register', [AuthController::class, 'register'])->name('register-send');
+Route::prefix('pelanggan')->group(function () {
+    Route::get('login', [PelangganAuthController::class, 'showLoginForm'])->name('login');
+    Route::post('login', [PelangganAuthController::class, 'login'])->name('login-send');
+    Route::get('register', [PelangganAuthController::class, 'showRegisterForm'])->name('register');
+    Route::post('register', [PelangganAuthController::class, 'register'])->name('register-send');
+    Route::get('forgot-password', [PelangganAuthController::class, 'showForgotPasswordForm'])->name('forgot-password');
+    Route::post('forgot-password', [PelangganAuthController::class, 'sendResetLink'])->name('forgot-password-send');
+    Route::get('reset-password/{token}', [PelangganAuthController::class, 'showResetPasswordForm'])->name('reset-password');
+    Route::post('reset-password', [PelangganAuthController::class, 'resetPassword'])->name('reset-password-send');
+    Route::post('logout', [PelangganAuthController::class, 'logout'])->name('logout');
+});
 
-Route::get('forgot-password', [AuthController::class, 'showForgotPasswordForm'])->name('forgot-password');
-Route::post('forgot-password', [AuthController::class, 'sendResetLink'])->name('forgot-password-send');
-Route::get('reset-password/{token}', [AuthController::class, 'showResetPasswordForm'])->name('reset-password');
-Route::post('reset-password', [AuthController::class, 'resetPassword'])->name('reset-password-send');
-Route::post('logout', [AuthController::class, 'logout'])->name('logout');
+Route::middleware('auth:pelanggan')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'show'])->name('profile');
+});
+
+Route::prefix('superadmin')->group(function () {
+    Route::get('login', [SuperAdminAuthController::class, 'showLogin'])->name('login-admin');
+    Route::post('login', [SuperAdminAuthController::class, 'login'])->name('login-admin-send');
+    Route::post('logout/admin', [SuperAdminAuthController::class, 'logout'])->name('logout-admin');
+});
 
 // UsedCar
 Route::get('used-car', [UsedCarController::class, 'index'])->name('used-car');
@@ -42,20 +53,21 @@ Route::get('faqs', [PageController::class, 'faqs'])->name('faqs');
 Route::get('terms', [PageController::class, 'terms'])->name('terms');
 Route::get('support-center', [PageController::class, 'supportCenter'])->name('support-center');
 Route::get('detail', [PageController::class, 'detail'])->name('detail');
-Route::get('login-admin', [SuperAdminAuthController::class, 'showLogin'])->name('login-admin');
-Route::post('login-admin', [SuperAdminAuthController::class, 'login'])->name('login-admin-send');
 Route::get('superadmin', [PageController::class, 'superAdmin'])->name('superadmin');
 Route::get('support-center-category', [SupportCenterController::class, 'category'])->name('support-center-category');
 
 
 // Profile route
-Route::get('/profile', [ProfileController::class, 'show'])->name('profile.show');
 Route::post('/profile/update', [ProfileController::class, 'updateProfile'])->name('profile.update');
 Route::get('/profile/address', [ProfileController::class, 'showAddress'])->name('profile.address');
 Route::get('/profile/address/create', [ProfileController::class, 'addAddress'])->name('profile.address.create');
 Route::post('/profile/address/store', [ProfileController::class, 'storeAddress'])->name('address.store');
 Route::get('profile/setting', [ProfileController::class, 'showSetting'])->name('profile.setting');
 Route::post('/profile/reset-password', [ProfileController::class, 'resetPassword'])->name('profile.resetPassword');
+Route::get('profile/used-car', [UsedCarController::class, 'showUsedCar'])->name('profile-used-car');
+Route::get('profile/used-car/create', [UsedCarController::class, 'create'])->name('used-car-create');
+Route::post('profile/used-car/store', [UsedCarController::class, 'store'])->name('used-car-store');
+
 
 // Event route
 Route::get('event', [EventController::class, 'show'])->name('event.show');
