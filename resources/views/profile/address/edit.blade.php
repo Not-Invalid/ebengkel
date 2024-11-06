03d3cebc404ea262a8b77abfeca7d6dd70f64859
dinoarr <dinoananda.restyanto@gmail.com>
2024-11-06 16:28:48 +0700

    feat: add and fix leaflet,api region in address

--- resources/views/profile/address/edit.blade.php
+++ resources/views/profile/address/edit.blade.php
@@ -3,6 +3,9 @@
 @section('title')
     eBengkelku | Edit Address
 @stop
+@push('css')
+    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css" />
+@endpush
 
 @section('content')
     <div class="w-100 shadow bg-white rounded" style="padding: 1rem">
@@ -11,25 +14,37 @@
             @csrf
 
             <div class="form-group mb-3">
-                <label for="nama_penerima">Nama Penerima</label>
-                <input type="text" class="form-control" name="nama_penerima" value="{{ $address->nama_penerima }}">
+                <div class="did-floating-label-content">
+                    <input class="did-floating-input" type="text" placeholder=" " id="name" name="nama_penerima"
+                        value="{{ $address->nama_penerima }}" />
+                    <label class="did-floating-label">Nama Penerima</label>
+                </div>
             </div>
 
             <div class="form-group mb-3">
-                <label for="telp_penerima">No Telp Penerima</label>
-                <input type="text" class="form-control" name="telp_penerima" required pattern="[0-9]*"
-                    oninput="this.value = this.value.replace(/[^0-9]/g, '');" value="{{ $address->telp_penerima }}">
+                <div class="did-floating-label-content">
+                    <input class="did-floating-input" type="text" placeholder=" " id="name" name="telp_penerima"
+                        pattern="[0-9]*" oninput="this.value = this.value.replace(/[^0-9]/g, '');"
+                        value="{{ $address->telp_penerima }}" />
+                    <label class="did-floating-label">No Telp Penerima</label>
+                </div>
             </div>
 
             <div class="form-group mb-3">
-                <label for="lokasi_alamat_pengiriman">Alamat</label>
-                <textarea class="form-control" name="lokasi_alamat_pengiriman">{{ $address->lokasi_alamat_pengiriman }}</textarea>
+                <div class="did-floating-label-content">
+                    <textarea class="did-floating-input form-control" name="lokasi_alamat_pengiriman" placeholder=" " rows="4"
+                        required style="height: 100px;resize: none">{{ $address->lokasi_alamat_pengiriman }}</textarea>
+                    <label class="did-floating-label">Alamat</label>
+                </div>
             </div>
 
             <div class="form-group mb-3">
-                <label for="kodepos_alamat_pengiriman">Kode Pos</label>
-                <input type="text" class="form-control" name="kodepos_alamat_pengiriman"
-                    value="{{ $address->kodepos_alamat_pengiriman }}">
+                <div class="did-floating-label-content">
+                    <input class="did-floating-input" type="text" placeholder=" " name="kodepos_alamat_pengiriman"
+                        value="{{ $address->kodepos_alamat_pengiriman }}" pattern="[0-9]*"
+                        oninput="this.value = this.value.replace(/[^0-9]/g, '');" />
+                    <label class="did-floating-label">Kode Pos</label>
+                </div>
             </div>
 
             <div class="form-group mb-3">
@@ -42,34 +57,42 @@
             </div>
 
             <div class="form-group mb-3">
-                <label for="provinsi">Provinsi</label>
-                <select class="form-control" id="provinsi" name="provinsi">
-                    <option value="{{ $address->provinsi }}" selected>{{ $address->provinsi }}</option>
-                </select>
+                <div class="did-floating-label-content">
+                    <select name="provinsi" id="provinsi" class="did-floating-select">
+                        <option value="{{ $address->provinsi }}" selected>{{ $address->provinsi }}</option>
+                    </select>
+                    <label class="did-floating-label">Provinsi</label>
+                </div>
             </div>
 
             <div class="form-group mb-3">
-                <label for="kota">Kota</label>
-                <select class="form-control" id="kota" name="kota">
-                    <option value="{{ $address->kota }}" selected>{{ $address->kota }}</option>
-                </select>
+                <div class="did-floating-label-content">
+                    <select name="kota" id="kota" class="did-floating-select">
+                        <option value="{{ $address->kota }}" selected>{{ $address->kota }}</option>
+                    </select>
+                    <label class="did-floating-label">Kota</label>
+                </div>
             </div>
 
             <div class="form-group mb-3">
-                <label for="kecamatan">Kecamatan</label>
-                <select class="form-control" id="kecamatan" name="kecamatan">
-                    <option value="{{ $address->kecamatan }}" selected>{{ $address->kecamatan }}</option>
-                </select>
+                <div class="did-floating-label-content">
+                    <select name="kecamatan" id="kecamatan" class="did-floating-select">
+                        <option value="{{ $address->kecamatan }}" selected>{{ $address->kecamatan }}</option>
+                    </select>
+                    <label class="did-floating-label">Kecamatan</label>
+                </div>
             </div>
 
             <div class="form-group mb-3">
-                <label>Status Alamat</label>
-                <select name="status_alamat_pengiriman" id="status_alamat_pengiriman" class="form-select">
-                    <option value="{{ $address->status_alamat_pengiriman }}" selected disabled hidden>
-                        {{ $address->status_alamat_pengiriman }}</option>
-                    <option value="Home">Home</option>
-                    <option value="Office">Office</option>
-                </select>
+                <div class="did-floating-label-content">
+                    <select name="status_alamat_pengiriman" id="status_alamat_pengiriman" class="did-floating-select">
+                        <option value="{{ $address->status_alamat_pengiriman }}" selected disabled hidden>
+                            {{ $address->status_alamat_pengiriman }}</option>
+                        <option value="office">Office</option>
+                        <option value="home">Home</option>
+                    </select>
+                    <label class="did-floating-label">Status Alamat</label>
+                </div>
             </div>
 
             <div>
@@ -78,21 +101,150 @@
             </div>
         </form>
     </div>
+
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js"></script>
+
     <script>
         document.addEventListener("DOMContentLoaded", function() {
-            var lat = {{ $address->lat_alamat_pengiriman }};
-            var lng = {{ $address->long_alamat_pengiriman }};
-            var map = L.map('map').setView([lat, lng], 13);
-            L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-                attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-            }).addTo(map);
-            var marker = L.marker([lat, lng]).addTo(map);
-            marker.on('dragend', function(e) {
-                var newLatLng = e.target.getLatLng();
-                document.getElementById('latitude').value = newLatLng.lat;
-                document.getElementById('longitude').value = newLatLng.lng;
-            });
-            marker.dragging.enable();
+            var lat = {{ $address->lat_alamat_pengiriman ?? 'null' }};
+            var lng = {{ $address->long_alamat_pengiriman ?? 'null' }};
+            var map, marker;
+
+            function initMap(latitude, longitude) {
+                map = L.map('map').setView([latitude, longitude], 13);
+                L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                }).addTo(map);
+                marker = L.marker([latitude, longitude], {
+                    draggable: true
+                }).addTo(map);
+                marker.on('dragend', function(e) {
+                    var newLatLng = e.target.getLatLng();
+                    document.getElementById('latitude').value = newLatLng.lat;
+                    document.getElementById('longitude').value = newLatLng.lng;
+                });
+            }
+
+            if (lat && lng) {
+                initMap(lat, lng);
+            } else if (navigator.geolocation) {
+                navigator.geolocation.getCurrentPosition(
+                    function(position) {
+                        initMap(position.coords.latitude, position.coords.longitude);
+                    },
+                    function(error) {
+                        console.error("Error getting location: " + error.message);
+                        initMap(-6.200000, 106.816666);
+                    }
+                );
+            } else {
+                initMap(-6.200000, 106.816666);
+            }
         });
     </script>
+
+    <script>
+        $(document).ready(function() {
+            let provinsiLoaded = false;
+
+            $('#provinsi').one('click', function() {
+                if (!provinsiLoaded) {
+                    $.get('https://api.cahyadsn.com/provinces', function(response) {
+                        let provinsiDropdown = $('#provinsi');
+                        provinsiDropdown.empty();
+                        provinsiDropdown.append(
+                            '<option value="" selected disabled hidden>Pilih Provinsi</option>');
+
+                        if (response.data && Array.isArray(response.data)) {
+                            $.each(response.data, function(index, provinsi) {
+                                provinsiDropdown.append('<option value="' + provinsi.kode +
+                                    '">' + provinsi.nama + '</option>');
+                            });
+                            provinsiLoaded = true;
+                        }
+                    }).fail(function() {
+                        console.log('Request gagal untuk data provinsi');
+                    });
+                }
+            });
+
+            $('#provinsi').change(function() {
+                let provinsiId = $(this).val();
+                if (provinsiId) {
+                    $.get('https://api.cahyadsn.com/regencies/' + provinsiId, function(response) {
+                        let kotaDropdown = $('#kota');
+                        kotaDropdown.empty();
+                        kotaDropdown.append(
+                            '<option value="" selected disabled hidden>Pilih Kota/Kabupaten</option>'
+                        );
+
+                        if (response.data && Array.isArray(response.data)) {
+                            $.each(response.data, function(index, kota) {
+                                kotaDropdown.append('<option value="' + kota.kode + '">' +
+                                    kota.nama + '</option>');
+                            });
+                        }
+                    }).fail(function() {
+                        console.log('Request gagal untuk data kota');
+                    });
+                } else {
+                    $('#kota').empty().append(
+                        '<option value="" selected disabled hidden>Pilih Kota/Kabupaten</option>');
+                    $('#kecamatan').empty().append(
+                        '<option value="" selected disabled hidden>Pilih Kecamatan</option>');
+                }
+            });
+
+            $('#kota').change(function() {
+                let kotaId = $(this).val();
+                if (kotaId) {
+                    $.get('https://api.cahyadsn.com/districts/' + kotaId, function(response) {
+                        let kecamatanDropdown = $('#kecamatan');
+                        kecamatanDropdown.empty();
+                        kecamatanDropdown.append(
+                            '<option value="" selected disabled hidden>Pilih Kecamatan</option>'
+                        );
+
+                        if (response.data && Array.isArray(response.data)) {
+                            $.each(response.data, function(index, kecamatan) {
+                                kecamatanDropdown.append('<option value="' + kecamatan
+                                    .kode + '">' + kecamatan.nama + '</option>');
+                            });
+                        }
+                    }).fail(function() {
+                        console.log('Request gagal untuk data kecamatan');
+                    });
+                } else {
+                    $('#kecamatan').empty().append(
+                        '<option value="" selected disabled hidden>Pilih Kecamatan</option>');
+                }
+            });
+
+            $('form').submit(function(event) {
+                var provinsiNama = $('#provinsi option:selected').text();
+                var kotaNama = $('#kota option:selected').text();
+                var kecamatanNama = $('#kecamatan option:selected').text();
+
+                $('<input>').attr({
+                    type: 'hidden',
+                    name: 'provinsi',
+                    value: provinsiNama
+                }).appendTo(this);
+
+                $('<input>').attr({
+                    type: 'hidden',
+                    name: 'kota',
+                    value: kotaNama
+                }).appendTo(this);
+
+                $('<input>').attr({
+                    type: 'hidden',
+                    name: 'kecamatan',
+                    value: kecamatanNama
+                }).appendTo(this);
+            });
+        });
+    </script>
+
 @endsection
