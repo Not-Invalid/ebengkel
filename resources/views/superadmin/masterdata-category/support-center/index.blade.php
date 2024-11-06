bf1897891514efd7ccff2813946516f1e4d83965
RaffDev31 <raffli4000@hotmail.com>
2024-11-06 15:58:19 +0700

    feat: update support center categories view

--- resources/views/superadmin/masterdata-category/support-center/index.blade.php
+++ resources/views/superadmin/masterdata-category/support-center/index.blade.php
@@ -1,43 +1,40 @@
 @extends('superadmin.layouts.app')
 
 @section('title')
-    eBengkelku | Categories
+    eBengkelku | Support Center Categories
 @stop
 
 @section('content')
     <div class="w-100 shadow bg-white rounded" style="padding: 1rem">
         <div class="d-flex justify-content-between align-items-center">
             <h4 class="fs-5">Support Center Categories</h4>
-            <a href="" class="btn btn-custom-2">+ Add New Categories</a>
+            <a href="{{ route('support-center-category-create') }}" class="btn btn-custom-2">+ Add New Categories</a>
         </div>
 
         @if ($categories->isEmpty())
             <div class="card border-1 rounded-2 mt-4">
                 <div class="card-body d-flex justify-content-center">
                     <div class="text-center">
-                        <img src="{{ asset('assets/images/components/empty.png') }}" height="130" width="130"
-                            alt="">
-                        <p>No data categories delivery.</p>
+                        <img src="{{ asset('assets/images/components/empty.png') }}" height="130" width="130" alt="">
+                        <p>Data not found.</p>
                     </div>
                 </div>
             </div>
         @else
-            @foreach ($categories as $categories)
-                @if ($categories)
+            @foreach ($categories as $category)
+                @if ($category)
                     <div class="card border-1 rounded-2 mt-4">
-                        <div class="card-body">
+                        <div class="card-body shadow-sm border">
                             <div class="row">
                                 <div class="col-12 col-md-3 d-flex align-items-center justify-content-center mb-3">
-                                    <div class="d-flex align-items-center">
-                                        <iframe
-                                           >
-                                        </iframe>
-                                    </div>
+                                    <i class="fas fa-{{ $category->icon }} icon-support-center"></i>
                                 </div>
-                                <div class="col-12 col-md-9">
-                                    <h5 class="card-title mt-3">Nama Kategori</h5>
-                                    <h6 class="card-title mt-3"></h6>
-                                    <a href="#" class="btn btn-custom-3">Edit</a>
+                                <div class="col-12 col-md-6">
+                                    <h5 class="card-title mt-3">{{ $category->nama_category }}</h5>
+                                    <p class="text-muted mt-1">{{ $category->icon }}</p>
+                                </div>
+                                <div class="col-12 col-md-3 d-flex align-items-center justify-content-center">
+                                    <a href="{{ route('support-center-category-edit', $category->id) }}" class="btn btn-custom-3">Edit</a>
                                 </div>
                             </div>
                         </div>
