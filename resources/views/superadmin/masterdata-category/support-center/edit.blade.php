606686bcbf20c19fa73fb4b091c8865ebcafa81e
RaffDev31 <raffli4000@hotmail.com>
2024-11-06 15:59:54 +0700

    feat: add new page for create and edit Support Center Category

--- resources/views/superadmin/masterdata-category/support-center/edit.blade.php
+++ resources/views/superadmin/masterdata-category/support-center/edit.blade.php
@@ -23,20 +23,25 @@
 
 @section('content')
     <div class="w-100 shadow bg-white rounded" style="padding: 1rem">
-        <h4>Add New Category</h4>
-        <form class="py-4" action="{{ route('support-center-category-send') }}" method="POST" enctype="multipart/form-data">
-            @csrf
+        <h4>Edit Category</h4>
 
+        <!-- Check for any success messages -->
+        @if(session('success'))
+            <div class="alert alert-success">{{ session('success') }}</div>
+        @endif
+
+        <form class="py-4" action="{{ route('support-center-category-update', $category->id) }}" method="POST" enctype="multipart/form-data">
+            @csrf
             <div class="row">
                 <div class="col">
                     <div class="did-floating-label-content">
-                        <input class="did-floating-input" type="text" placeholder=" " id="nama_category" name="nama_category" />
+                        <input class="did-floating-input" type="text" placeholder=" " id="nama_category" name="nama_category" value="{{ old('nama_category', $category->nama_category) }}" />
                         <label class="did-floating-label">Nama Kategori</label>
                     </div>
                 </div>
                 <div class="col">
                     <div class="did-floating-label-content">
-                        <input class="did-floating-input" type="text" placeholder=" " id="icon" name="icon" />
+                        <input class="did-floating-input" type="text" placeholder=" " id="icon" name="icon" value="{{ old('icon', $category->icon) }}" oninput="updateIconPreview()" />
                         <label class="did-floating-label">Icon</label>
                     </div>
                     <small class="form-text text-muted mt-1">
@@ -44,7 +49,7 @@
                     </small>
                     <div class="mt-2">
                         <span>Icon Preview:</span>
-                        <i id="icon-preview" class="fas"></i>
+                        <i id="icon-preview" class="fas fa-{{ old('icon', $category->icon) }}"></i>
                     </div>
                 </div>
             </div>
@@ -55,3 +60,16 @@
         </form>
     </div>
 @endsection
+
+@push('js')
+<script>
+    // Function to update the icon preview
+    function updateIconPreview() {
+        var iconClass = document.getElementById('icon').value;  // Get the input value
+        var iconPreview = document.getElementById('icon-preview');  // Get the icon preview element
+
+        // Dynamically update the class of the icon preview element by prepending "fas fa-" to the input value
+        iconPreview.className = "fas fa-" + iconClass;
+    }
+</script>
+@endpush
