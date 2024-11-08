8d2006408324bddcf10699d97a773496d0713226
Fawasdiba <fawasabid@gmail.com>
2024-11-08 16:35:31 +0700

    feat:add update create profil usedCar

--- resources/views/profile/used-car/create.blade.php
+++ resources/views/profile/used-car/create.blade.php
@@ -3,7 +3,46 @@
 @section('title')
     eBengkelku | Create Used Car
 @stop
+<style>
+    .image-preview {
+        border: 1px solid #ddd;
+        border-radius: 5px;
+        max-height: 200px;
+    }
 
+    .upload-box {
+        border: 2px dashed var(--main-light-blue);
+        border-radius: 8px;
+        padding: 20px;
+        text-align: center;
+        transition: background-color 0.2s;
+        cursor: pointer;
+    }
+
+    .upload-box:hover {
+        background-color: #f9f9f9;
+    }
+
+    .upload-label {
+        font-size: 16px;
+        color: #555;
+        margin-bottom: 10px;
+        display: block;
+    }
+
+    .file-input {
+        opacity: 0;
+        position: absolute;
+        z-index: -1;
+    }
+
+    .upload-box::after {
+        content: 'Click to upload';
+        display: block;
+        font-size: 14px;
+        color: #999;
+    }
+</style>
 @section('content')
     <div class="w-100 shadow bg-white rounded" style="padding: 1rem">
         <h4>Add Used Car</h4>
@@ -11,13 +50,13 @@
             @csrf
 
             <div class="row">
-                <div class="col">
+                <div class="col-md-6">
                     <div class="did-floating-label-content">
                         <input class="did-floating-input" type="text" placeholder=" " id="nama_mobil" name="nama_mobil" />
                         <label class="did-floating-label car">Nama Mobil</label>
                     </div>
                 </div>
-                <div class="col">
+                <div class="col-md-6">
                     <div class="did-floating-label-content">
                         <input class="did-floating-input" type="text" placeholder=" " id="merk_mobil"
                             name="merk_mobil" />
@@ -27,14 +66,14 @@
             </div>
 
             <div class="row">
-                <div class="col">
+                <div class="col-md-6">
                     <div class="did-floating-label-content">
                         <input class="did-floating-input" type="text" placeholder=" " id="harga_mobil"
                             name="harga_mobil" />
                         <label class="did-floating-label car">Harga Mobil</label>
                     </div>
                 </div>
-                <div class="col">
+                <div class="col-md-6">
                     <div class="did-floating-label-content">
                         <input class="did-floating-input" type="text" placeholder=" " id="plat_nomor"
                             name="plat_nomor_mobil" />
@@ -44,14 +83,14 @@
             </div>
 
             <div class="row">
-                <div class="col">
+                <div class="col-md-6">
                     <div class="did-floating-label-content">
                         <input class="did-floating-input" type="text" placeholder=" " id="tahun_mobil"
                             name="tahun_mobil" />
                         <label class="did-floating-label car">Tahun Mobil</label>
                     </div>
                 </div>
-                <div class="col">
+                <div class="col-md-6">
                     <div class="did-floating-label-content">
                         <input class="did-floating-input" type="text" placeholder=" " id="km_mobil" name="km_mobil" />
                         <label class="did-floating-label car">KM Mobil</label>
@@ -60,14 +99,14 @@
             </div>
 
             <div class="row">
-                <div class="col">
+                <div class="col-md-6">
                     <div class="did-floating-label-content">
                         <input class="did-floating-input" type="text" placeholder=" " id="nomor_rangka_mobil"
                             name="nomor_rangka_mobil" />
                         <label class="did-floating-label car">Nomor Rangka Mobil</label>
                     </div>
                 </div>
-                <div class="col">
+                <div class="col-md-6">
                     <div class="did-floating-label-content">
                         <input class="did-floating-input" type="text" placeholder=" " id="nomor_mesin"
                             name="nomor_mesin_mobil" />
@@ -77,17 +116,17 @@
             </div>
 
             <div class="row">
-                <div class="col">
+                <div class="col-md-6">
                     <div class="did-floating-label-content">
                         <input class="did-floating-input" type="text" placeholder=" " id="kapasitas_mesin_mobil"
                             name="kapasitas_mesin_mobil" />
                         <label class="did-floating-label car">Kapasitas Mesin Mobil</label>
                     </div>
                 </div>
-                <div class="col">
+                <div class="col-md-6">
                     <div class="did-floating-label-content">
                         <select class="did-floating-input" id="bahan_bakar" name="bahan_bakar_mobil" required>
-                            <option value="" disabled selected hidden></option>
+                            <option value="" disabled selected hidden>Pilih bahan bakar</option>
                             <option value="bensin">Bensin</option>
                             <option value="diesel">Diesel</option>
                             <option value="listrik">Listrik</option>
@@ -99,14 +138,17 @@
             </div>
 
             <div class="row">
-                <div class="col">
+                <div class="col-md-6">
                     <div class="did-floating-label-content">
-                        <input class="did-floating-input" type="text" placeholder=" " id="jenis_transmisi_mobil"
-                            name="jenis_transmisi_mobil" />
-                        <label class="did-floating-label car">Jenis Transmisi Mobil</label>
+                        <select class="did-floating-input" id="transmisi_mobil" name="jenis_transmisi_mobil" required>
+                            <option value="" disabled selected hidden>Pilih transmisi</option>
+                            <option value="matic">Matic</option>
+                            <option value="manual">Manual</option>
+                        </select>
+                        <label class="did-floating-label">Jenis Transmisi Mobil</label>
                     </div>
                 </div>
-                <div class="col">
+                <div class="col-md-6">
                     <div class="did-floating-label-content">
                         <input class="did-floating-input" type="month" placeholder=" " id="bulan_pajak_mobil"
                             name="bulan_pajak_mobil" />
@@ -116,14 +158,14 @@
             </div>
 
             <div class="row">
-                <div class="col">
+                <div class="col-md-6">
                     <div class="did-floating-label-content">
                         <input class="did-floating-input" type="text" placeholder=" " id="tahun_pajak_mobil"
                             name="tahun_pajak_mobil" />
                         <label class="did-floating-label car">Tahun Pajak Mobil</label>
                     </div>
                 </div>
-                <div class="col">
+                <div class="col-md-6">
                     <div class="did-floating-label-content">
                         <input class="did-floating-input" type="date" placeholder=" " id="terakhir_pajak_mobil"
                             name="terakhir_pajak_mobil" />
@@ -150,16 +192,105 @@
             </div>
 
             <div class="form-group mb-3">
-                <label for="foto_mobil">Upload Foto Mobil</label>
-                <input type="file" class="form-control" name="file_foto_mobil" required>
-                <input type="file" class="form-control mt-2" name="file_foto_mobil_2">
-                <input type="file" class="form-control mt-2" name="file_foto_mobil_3">
-                <input type="file" class="form-control mt-2" name="file_foto_mobil_4">
-                <input type="file" class="form-control mt-2" name="file_foto_mobil_5">
+                <label for="foto_mobil" class="mb-2">Upload Foto Mobil</label>
+
+                <!-- Foto Mobil 1 -->
+                <div class="col-md-12">
+                    <label for="file_foto_mobil_1" class="upload-label">
+                        <div class="upload-box">
+                            <div class="preview-container d-flex justify-content-center align-items-center"
+                                onclick="triggerFileInput('file_foto_mobil_1')">
+                                <img id="foto1Preview" src="#" alt="Foto Mobil 1 Preview" class="image-preview"
+                                    style="display: none; width: 200px; margin-top: 10px; cursor: pointer;">
+                                <i class="fa-solid fa-plus" style="font-size: 36px;"></i>
+                                <p class="mb-2 text-sm text-gray-500">
+                                </p>
+                            </div>
+                        </div>
+                    </label>
+                    <input type="file" class="file-input" name="file_foto_mobil_1" id="file_foto_mobil_1"
+                        onchange="previewImage('file_foto_mobil_1', 'foto1Preview')" style="display: none;"
+                        accept="image/*">
+                </div>
+
+                <!-- Foto Mobil 2 -->
+                <div class="col-md-12">
+                    <label for="file_foto_mobil_2" class="upload-label">
+                        <div class="upload-box mt-5">
+                            <div class="preview-container d-flex justify-content-center"
+                                onclick="triggerFileInput('file_foto_mobil_2')">
+                                <img id="foto2Preview" src="#" alt="Foto Mobil 2 Preview" class="image-preview"
+                                    style="display: none; width: 200px; margin-top: 10px; cursor: pointer;">
+                                <i class="fa-solid fa-plus" style="font-size: 36px;"></i>
+                            </div>
+                        </div>
+                    </label>
+                    <input type="file" class="file-input" name="file_foto_mobil_2" id="file_foto_mobil_2"
+                        onchange="previewImage('file_foto_mobil_2', 'foto2Preview')" style="display: none;"
+                        accept="image/*">
+                </div>
+
+                <!-- Foto Mobil 3 -->
+                <div class="col-md-12">
+                    <label for="file_foto_mobil_3" class="upload-label">
+                        <div class="upload-box mt-5">
+                            <div class="preview-container d-flex justify-content-center"
+                                onclick="triggerFileInput('file_foto_mobil_3')">
+                                <img id="foto3Preview" src="#" alt="Foto Mobil 3 Preview" class="image-preview"
+                                    style="display: none; width: 200px; margin-top: 10px; cursor: pointer;">
+                                <i class="fa-solid fa-plus" style="font-size: 36px;"></i>
+                            </div>
+                        </div>
+                    </label>
+                    <input type="file" class="file-input" name="file_foto_mobil_3" id="file_foto_mobil_3"
+                        onchange="previewImage('file_foto_mobil_3', 'foto3Preview')" style="display: none;"
+                        accept="image/*">
+                </div>
+
+                <!-- Foto Mobil 4 -->
+                <div class="col-md-12">
+                    <label for="file_foto_mobil_4" class="upload-label">
+                        <div class="upload-box mt-5">
+                            <div class="preview-container d-flex justify-content-center"
+                                onclick="triggerFileInput('file_foto_mobil_4')">
+                                <img id="foto4Preview" src="#" alt="Foto Mobil 4 Preview" class="image-preview"
+                                    style="display: none; width: 200px; margin-top: 10px; cursor: pointer;">
+                                <i class="fa-solid fa-plus" style="font-size: 36px;"></i>
+                            </div>
+                        </div>
+                    </label>
+                    <input type="file" class="file-input" name="file_foto_mobil_4" id="file_foto_mobil_4"
+                        onchange="previewImage('file_foto_mobil_4', 'foto4Preview')" style="display: none;"
+                        accept="image/*">
+                </div>
+
+                <!-- Foto Mobil 5 -->
+                <div class="col-md-12">
+                    <label for="file_foto_mobil_5" class="upload-label">
+                        <div class="upload-box mt-5">
+                            <div class="preview-container d-flex justify-content-center"
+                                onclick="triggerFileInput('file_foto_mobil_5')">
+                                <img id="foto5Preview" src="#" alt="Foto Mobil 5 Preview" class="image-preview"
+                                    style="display: none; width: 200px; margin-top: 10px; cursor: pointer;">
+                                <i class="fa-solid fa-plus" style="font-size: 36px;"></i>
+                            </div>
+                        </div>
+                    </label>
+                    <input type="file" class="file-input" name="file_foto_mobil_5" id="file_foto_mobil_5"
+                        onchange="previewImage('file_foto_mobil_5', 'foto5Preview')" style="display: none;"
+                        accept="image/*">
+                </div>
             </div>
-            <button type="submit" class="btn btn-custom-icon mt-3">
-                <i class='bx bxs-save fs-5'></i> Simpan
-            </button>
+
+            <div class="d-flex justify-content-start mt-3">
+                <!-- Tombol Simpan -->
+                <button type="submit" class="btn btn-custom-icon me-2">
+                    Simpan
+                </button>
+
+                <!-- Tombol Back yang berada di sebelah kanan -->
+                <a href="{{ route('profile-used-car') }}" class="btn btn-danger">Back</a>
+            </div>
         </form>
     </div>
 
@@ -169,4 +300,30 @@
             this.value = this.value.replace(/[^0-9]/g, '').slice(0, 4);
         });
     </script>
+    <script>
+        function triggerFileInput(fileInputId) {
+            // Trigger file input click
+            document.getElementById(fileInputId).click();
+        }
+
+        function previewImage(inputId, previewId) {
+            var fileInput = document.getElementById(inputId);
+            var previewImage = document.getElementById(previewId);
+
+            // Pastikan ada file yang dipilih
+            if (fileInput.files && fileInput.files[0]) {
+                var reader = new FileReader();
+
+                reader.onload = function(e) {
+                    // Tampilkan gambar pada preview
+                    previewImage.style.display = "block";
+                    previewImage.src = e.target.result;
+                };
+
+                // Baca file yang dipilih
+                reader.readAsDataURL(fileInput.files[0]);
+            }
+        }
+    </script>
+
 @endsection
