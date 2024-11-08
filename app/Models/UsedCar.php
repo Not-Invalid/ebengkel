b095ce212a2f62427c337781d41ee50c214c15b7
Fawasdiba <fawasabid@gmail.com>
2024-11-08 16:29:34 +0700

    feat:add update model usedCar function fotos

--- app/Models/UsedCar.php
+++ app/Models/UsedCar.php
@@ -14,19 +14,37 @@ class UsedCar extends Model
     public $timestamps = false;
 
     protected $fillable = [
-        'id_pelanggan', 'nama_mobil', 'merk_mobil', 'harga_mobil',
-        'tahun_mobil', 'plat_nomor_mobil', 'nomor_rangka_mobil',
-        'nomor_mesin_mobil', 'kapasitas_mesin_mobil', 'bahan_bakar_mobil',
-        'jenis_transmisi_mobil', 'km_mobil', 'bulan_pajak_mobil',
-        'tahun_pajak_mobil', 'terakhir_service_mobil',
-        'terakhir_pajak_mobil', 'keterangan_mobil', 'lokasi_mobil',
-        'kodepos_mobil', 'lat_mobil', 'long_mobil', 'approv_mobil',
-        'status_mobil', 'sold_out_mobil', 'create_date', 'delete_mobil'
+        'id_pelanggan',
+        'nama_mobil',
+        'merk_mobil',
+        'harga_mobil',
+        'tahun_mobil',
+        'plat_nomor_mobil',
+        'nomor_rangka_mobil',
+        'nomor_mesin_mobil',
+        'kapasitas_mesin_mobil',
+        'bahan_bakar_mobil',
+        'jenis_transmisi_mobil',
+        'km_mobil',
+        'bulan_pajak_mobil',
+        'tahun_pajak_mobil',
+        'terakhir_service_mobil',
+        'terakhir_pajak_mobil',
+        'keterangan_mobil',
+        'lokasi_mobil',
+        'kodepos_mobil',
+        'lat_mobil',
+        'long_mobil',
+        'approv_mobil',
+        'status_mobil',
+        'sold_out_mobil',
+        'create_date',
+        'delete_mobil'
     ];
 
     public function fotos()
     {
-        return $this->hasMany(FotoMobil::class, 'id_mobil', 'id_mobil');
+        return $this->hasOne(FotoMobil::class, 'id_mobil', 'id_mobil');
     }
 
     public function pelanggan()
