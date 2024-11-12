f6ea20f748462b156666ca362723e57bbd6184ca
sidaoaa <muhammaddaffaarsiladzim@gmail.com>
2024-11-12 11:16:18 +0700

    update service models

---
+++ app/Models/Service.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Service extends Model
+{
+    protected $table = 'tb_services';
+    protected $primaryKey = 'id_services';
+    public $incrementing = true;
+    protected $keyType = 'integer';
+    public $timestamps = false;
+    protected $fillable = [
+        'id_bengkel',
+        'nama_services',
+        'keterangan_services',
+        'harga_services',
+        'foto_services',
+        'delete_services',
+    ];
+
+    /**
+     * 
+     */
+    public function bengkel()
+    {
+        return $this->belongsTo(Bengkel::class, 'id_bengkel');
+    }
+}
