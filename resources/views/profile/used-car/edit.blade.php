bf5da3afd6efe70648010acc1b00ddf72a22d7a9
Fawasdiba <fawasabid@gmail.com>
2024-11-12 09:46:36 +0700

    fead:add get old merk mobil in editblade usedcar

--- resources/views/profile/used-car/edit.blade.php
+++ resources/views/profile/used-car/edit.blade.php
@@ -63,11 +63,12 @@
                 <div class="col-md-6">
                     <div class="did-floating-label-content">
                         <select class="did-floating-input" id="merk_mobil_id" name="merk_mobil_id" required>
-                            <option value="{{ old('merk_mobil', $car->merk_mobil) }}" disabled selected hidden>Pilih Merk
-                                Mobil</option>
+                            <option value="" disabled hidden>Pilih Merk Mobil</option>
                             @foreach ($carMerks as $merk)
-                                <option value="{{ old('merk_mobil', $car->merk_mobil) }}{{ $merk->id }}">
-                                    {{ $merk->nama_merk }}</option>
+                                <option value="{{ $merk->id }}"
+                                    {{ old('merk_mobil_id', $mobil->merk_mobil_id) == $merk->id ? 'selected' : '' }}>
+                                    {{ $merk->nama_merk }}
+                                </option>
                             @endforeach
                         </select>
                         <label class="did-floating-label">Merk Mobil</label>
@@ -113,8 +114,7 @@
                 <div class="col-md-6">
                     <div class="did-floating-label-content">
                         <input class="did-floating-input" type="text" placeholder=" " id="nomor_rangka_mobil"
-                            name="nomor_rangka_mobil"
-                            value="{{ old('nomor_rangka_mobil', $mobil->nomor_rangka_mobil) }}" />
+                            name="nomor_rangka_mobil" value="{{ old('nomor_rangka_mobil', $mobil->nomor_rangka_mobil) }}" />
                         <label class="did-floating-label car">Nomor Rangka Mobil</label>
                     </div>
                 </div>
