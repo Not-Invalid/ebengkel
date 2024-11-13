5fc188274d3fa68a12761aa5d1466fde23fea70b
RaffDev31 <raffli4000@hotmail.com>
2024-11-13 15:46:07 +0700

    feat: add new page for Daftar Peserta Event

---
+++ resources/views/superadmin/event/daftar_peserta.blade.php
@@ -0,0 +1,74 @@
+@extends('superadmin.layouts.app')
+
+@section('title')
+    eBengkelku | Daftar Peserta
+@stop
+
+@section('content')
+    <div class="container mt-4">
+        <div class="d-flex justify-content-between align-items-center mb-3">
+            <h2>Daftar Peserta Event{{ $event->nama_event }}</h2>
+        </div>
+
+        <table class="table table-bordered">
+            <thead class="field-title">
+                <tr>
+                    <th>No</th>
+                    <th>Nama</th>
+                    <th>Email</th>
+                    <th>No Telepon</th>
+                    <th>Payment Status</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach($daftar_peserta as $index => $data)
+                    <tr>
+                        <td>{{ $index + 1 }}</td>
+                        <td>{{ $data->nama_peserta }}</td>
+                        <td>{{ $data->email }}</td>
+                        <td>{{ $data->no_telepon }}</td>
+                        <td>{{ $data->payment_status == 'Y' ? 'Paid' : 'Not Paid' }}</td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+
+    <div class="d-flex justify-content-end mt-4">
+        <nav aria-label="Page navigation">
+            <ul class="pagination">
+                @if ($daftar_peserta->onFirstPage())
+                    <li class="page-item disabled">
+                        <span class="page-link bg-light border-0 rounded-pill"><i class="fas fa-chevron-left"></i></span>
+                    </li>
+                @else
+                    <li class="page-item">
+                        <a href="{{ $daftar_peserta->previousPageUrl() }}" class="page-link bg-light border-0 rounded-pill hover:bg-danger text-dark"><i class="fas fa-chevron-left"></i></a>
+                    </li>
+                @endif
+
+                @foreach ($daftar_peserta->getUrlRange(1, $daftar_peserta->lastPage()) as $page => $url)
+                    @if ($page == $daftar_peserta->currentPage())
+                        <li class="page-item active">
+                            <span class="page-num page-link text-white border-0 rounded-pill">{{ $page }}</span>
+                        </li>
+                    @else
+                        <li class="page-item">
+                            <a href="{{ $url }}" class="page-link bg-light border-0 rounded-pill hover:bg-danger text-dark">{{ $page }}</a>
+                        </li>
+                    @endif
+                @endforeach
+
+                @if ($daftar_peserta->hasMorePages())
+                    <li class="page-item">
+                        <a href="{{ $daftar_peserta->nextPageUrl() }}" class="page-link bg-light border-0 rounded-pill hover:bg-danger text-dark"><i class="fas fa-chevron-right"></i></a>
+                    </li>
+                @else
+                    <li class="page-item disabled">
+                        <span class="page-link bg-light border-0 rounded-pill"><i class="fas fa-chevron-right"></i></span>
+                    </li>
+                @endif
+            </ul>
+        </nav>
+    </div>
+@endsection
