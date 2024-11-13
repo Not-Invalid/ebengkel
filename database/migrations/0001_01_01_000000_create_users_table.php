13f2ce1613d80bc3149ee6a5309e80a4c85ed65f
RaffDev31 <raffli4000@hotmail.com>
2024-11-13 15:39:31 +0700

    feat(migration): add new field at users schema

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
+            $table->string('phone_number');
             $table->enum('role', ['Administrator', 'User']);
             $table->string('password');
             $table->text('foto_profile')->nullable();
