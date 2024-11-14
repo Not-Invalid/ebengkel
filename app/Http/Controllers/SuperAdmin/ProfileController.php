27294c40045cfd5dd96e3003f6db83d69dcf8406
RaffDev31 <raffli4000@hotmail.com>
2024-11-14 10:06:30 +0700

    feat(profile): add new method for update profile superadmin

--- app/Http/Controllers/SuperAdmin/ProfileController.php
+++ app/Http/Controllers/SuperAdmin/ProfileController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\SuperAdmin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class ProfileController extends Controller
@@ -13,4 +14,36 @@ class ProfileController extends Controller
         $superadmin = Auth::user();
         return view('superadmin.profile.index', compact('superadmin'));
     }
+
+    public function update(Request $request, $id)
+    {
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|email|max:255',
+            'phone_number' => 'nullable|string|max:15',
+            'foto_profile' => 'nullable|image|mimes:jpeg,png,jpg|max:2048'
+        ]);
+
+        $user = User::findOrFail($id);
+
+        $user->name = $request->input('name');
+        $user->email = $request->input('email');
+        $user->phone_number = $request->input('phone_number');
+
+        if ($request->hasFile('foto_profile')) {
+            $image = $request->file('foto_profile');
+
+            $imageName = 'foto_admin_' . date('Ymd_His') . '.' . $image->getClientOriginalExtension();
+
+            $image->move(public_path('assets/images/profile_picture'), $imageName);
+
+            $user->foto_profile = url('assets/images/profile_picture/' . $imageName);
+        }
+
+        $user->save();
+
+        return redirect()->route('profile', $id)
+                        ->with('success', 'Profile updated successfully.');
+    }
+
 }
