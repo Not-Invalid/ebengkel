3bf358348ee7f3ecbd7559dc43c841765f15ec26
RaffDev31 <raffli4000@hotmail.com>
2024-11-14 15:28:16 +0700

    feat(model): add new fillable field

--- app/Models/User.php
+++ app/Models/User.php
@@ -23,7 +23,8 @@ class User extends Authenticatable
         'password',
         'phone_number',
         'role',
-        'foto_profile'
+        'foto_profile',
+        'password'
     ];
 
     /**
