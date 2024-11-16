69165ff8f270390a69c9b076a0d15f1647e9ec40
RaffDev31 <raffli4000@hotmail.com>
2024-11-16 11:41:25 +0700

    feat(settings): add new controller for settings menu in superadmin

---
+++ app/Http/Controllers/SuperAdmin/SettingsController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers\SuperAdmin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Session;
+use App\Models\User;
+
+class SettingsController extends Controller
+{
+    public function index()
+    {
+        return view('superadmin.settings.change-password.index');
+    }
+
+    public function resetPassword(Request $request)
+    {
+        $request->validate([
+            'currentPassword' => 'required|string',
+            'newPassword' => 'required|string|min:8|confirmed',
+        ]);
+
+        $user = User::find(Auth::id());
+
+        if (!$user) {
+            return back()->with('status_error', 'User not found.');
+        }
+
+        if (!password_verify($request->input('currentPassword'), $user->password)) {
+            return back()->with('status_error', 'Current password is incorrect.');
+        }
+
+        $user->update([
+            'password' => Hash::make($request->input('newPassword')),
+        ]);
+
+        Auth::logout();
+
+        session()->invalidate();
+        session()->regenerateToken();
+
+        return redirect()->route('login-admin')->with('status', 'Password successfully changed! Please login again.');
+    }
+
+}
