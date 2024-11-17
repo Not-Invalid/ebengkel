1e93eccf5e4f0f7d339b83a94445345b4219a206
RaffDev31 <raffli4000@hotmail.com>
2024-11-17 13:19:47 +0700

    feat(models): add new model for cart

---
+++ app/Models/Cart.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use App\Models\Pelanggan;
+use App\Models\Product;
+
+class Cart extends Model
+{
+    protected $table = 'tb_cart';
+
+    protected $fillable = [
+        'id_pelanggan',
+        'id_produk',
+        'quantity',
+        'total_price',
+    ];
+
+    public function pelanggan()
+    {
+        return $this->belongsTo(Pelanggan::class, 'id_pelanggan');
+    }
+
+    public function produk()
+    {
+        return $this->belongsTo(Product::class, 'id_produk');
+    }
+}
