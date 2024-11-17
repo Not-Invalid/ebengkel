e2366be4d8dac0acce3014b021234501d5cb6357
RaffDev31 <raffli4000@hotmail.com>
2024-11-17 13:20:10 +0700

    feat(migrations) add new migration for tb_cart

---
+++ database/migrations/2024_11_17_051845_tb_cart.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tb_cart', function (Blueprint $table) {
+            $table->id();
+            $table->integer('id_pelanggan');
+            $table->unsignedInteger('id_produk');
+            $table->integer('quantity')->default(1);
+            $table->integer('total_price');
+            $table->timestamps();
+
+            $table->foreign('id_pelanggan')->references('id_pelanggan')->on('tb_pelanggan')->onDelete('cascade');
+            $table->foreign('id_produk')->references('id_produk')->on('tb_produk')->onDelete('cascade');
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tb_cart');
+    }
+};
