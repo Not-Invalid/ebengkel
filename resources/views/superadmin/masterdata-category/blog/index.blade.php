995c3203c9eed12703ec2083c0e737325f106a06
RaffDev31 <raffli4000@hotmail.com>
2024-11-19 15:54:57 +0700

    feat(blog): add new blade view for kategori blog in superadmin

---
+++ resources/views/superadmin/masterdata-category/blog/index.blade.php
@@ -0,0 +1,47 @@
+@extends('superadmin.layouts.app')
+
+@section('title')
+    eBengkelku | Blog Categories
+@stop
+
+@section('content')
+    <div class="container mt-4">
+        <div class="d-flex justify-content-between align-items-center mb-3">
+            <h2>Blog Categories</h2>
+            <a href="{{ route('blog-category-create') }}" class="btn btn-custom-2 btn-sm">
+                <i class="fas fa-plus"></i> Tambah Kategori Baru
+            </a>
+        </div>
+
+        <table class="table table-bordered">
+            <thead class="field-title">
+                <tr>
+                    <th>No</th>
+                    <th>Nama Kategori</th>
+                    <th>Action</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach($category as $data)
+                    <tr>
+                        <td>{{ $loop->iteration }}</td>
+                        <td>{{ $data->nama_kategori }}</td>
+                        <td class="text-center">
+                            <a href="{{ route('blog-category-edit', $data->id) }}" class="btn btn-custom-3 my-2" title="Edit" category-bs-toggle="tooltip">
+                                <i class="fas fa-edit text-primary"></i>
+                            </a>
+
+                            <form action="{{ route('blog-category-delete', $data->id) }}" method="POST" style="display:inline-block;">
+                                @csrf
+                                @method('DELETE')
+                                <button type="submit" class="btn btn-delete" title="Delete" data-bs-toggle="tooltip" style="border: none; background: none;">
+                                    <i class="fas fa-trash-alt text-white"></i>
+                                </button>
+                            </form>
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+@endsection
