3c3e91448ee89efa0bddefe708ff85e232da6455
RaffDev31 <raffli4000@hotmail.com>
2024-11-20 13:11:45 +0700

    feat: add blog listing and detail functionality with categories

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -3,11 +3,28 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Blog;
+use App\Models\KategoriBlog;
 
 class BlogController extends Controller
 {
     public function index()
     {
-        return view('blog.index');
+        $blogs = Blog::with('kategori')->latest()->get();
+        $kategoriBlogs = KategoriBlog::all();
+
+        return view('blog.index', compact('blogs', 'kategoriBlogs'));
     }
+
+    public function show($judul)
+    {
+        // Cari blog berdasarkan judul yang sudah diubah menjadi slug
+        $blog = Blog::where('slug', $judul)->firstOrFail();  // Menggunakan 'slug' untuk mencari blog
+
+        // Mengembalikan tampilan dengan data blog
+        return view('blog.detail', compact('blog'));
+    }
+
+
+
 }
