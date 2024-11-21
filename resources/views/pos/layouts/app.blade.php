831e74a5cef55363dae4ea291fbbc81f8babfde4
dinoarr <dinoananda.restyanto@gmail.com>
2024-11-21 16:33:16 +0700

    fix: sidebar and dropdown navbar

--- resources/views/pos/layouts/app.blade.php
+++ resources/views/pos/layouts/app.blade.php
@@ -7,31 +7,45 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="description" content="Bengkel Service, Spare Part & Smart Tools.">
     <link rel="shortcut icon" href="{{ asset('assets/images/logo/icon.png') }}" type="image/x-icon" />
+    <title>@yield('title')</title>
+
+    {{-- Bootstrap --}}
+    <link rel="stylesheet" href="{{ asset('template_pos/modules/bootstrap/css/bootstrap.min.css') }}" />
 
     {{-- Toastr CSS --}}
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css"
         integrity="sha512-vKMx8UnXk60zUwyUnUPM3HbQo8QfmNx7+ltw8Pm5zLusl1XIfwcxo8DbWCqMGKaWeNxWA8yrx5v3SaVpMvR3CA=="
         crossorigin="anonymous" referrerpolicy="no-referrer" />
 
-    {{-- font awesome --}}
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.0/css/all.min.css"
-        integrity="sha512-9xKTRVabjVeZmc+GUW8GgSmcREDunMM+Dt/GrzchfN8tkwHizc5RP4Ok/MXFFy5rIjJjzhndFScTceq5e6GvVQ=="
-        crossorigin="anonymous" referrerpolicy="no-referrer" />
-
-
-    {{-- Bootstrap --}}
-    <link rel="stylesheet" href="{{ asset('template_pos/modules/bootstrap/css/bootstrap.min.css') }}">
+    {{-- Font awesome --}}
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
 
     {{-- Aos --}}
     <link href="https://cdnjs.cloudflare.com/ajax/libs/aos/2.3.4/aos.css" rel="stylesheet">
 
-    {{-- Custom styles --}}
+    {{-- Css libraries --}}
+    <link rel="stylesheet" href="{{ asset('template_pos/modules/jqvmap/dist/jqvmap.min.css') }}" />
+    <link rel="stylesheet" href="{{ asset('template_pos/modules/weather-icon/css/weather-icons.min.css') }}" />
+    <link rel="stylesheet" href="{{ asset('template_pos/modules/weather-icon/css/weather-icons-wind.min.css') }}" />
+    <link rel="stylesheet" href="{{ asset('template_pos/modules/summernote/summernote-bs4.css') }}" />
+
+    {{-- Template and Custom css --}}
     <link rel="stylesheet" href="{{ asset('assets/css/custom.css') }}">
-    <link rel="stylesheet" href="{{ asset('template_pos/css/style.css') }}">
-    <link rel="stylesheet" href="{{ asset('template_pos/css/components.css') }}">
-    <link rel="stylesheet" href="{{ asset('template_pos/modules/summernote/summernote-bs4.css') }}">
+    <link rel="stylesheet" href="{{ asset('template_pos/css/style.css') }}" />
+    <link rel="stylesheet" href="{{ asset('template_pos/css/components.css') }}" />
+    <!-- Start GA -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-94034622-3"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
 
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        gtag("js", new Date());
 
+        gtag("config", "UA-94034622-3");
+    </script>
+    <!-- /END GA -->
 
     {{-- Poppins font --}}
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins:400,500,600,700,800">
@@ -46,12 +60,18 @@
             background-color: #bd362f !important;
             color: #fff !important;
         }
+
+        #toast-container>.toast:before {
+            font-family: FontAwesome;
+            content: "\f00c";
+            margin-right: 10px;
+        }
     </style>
     <title>@yield('title')</title>
-
 </head>
 
 <body class="@yield('body-class')">
+
     {{-- Loader --}}
     <div id="loader">
         <div id="center">
@@ -76,94 +96,28 @@
                             <a href="#" data-toggle="sidebar" class="nav-link nav-link-lg"><i
                                     class="fas fa-bars"></i></a>
                         </li>
-                        <li>
-                            <a href="#" data-toggle="search" class="nav-link nav-link-lg d-sm-none"><i
-                                    class="fas fa-search"></i></a>
-                        </li>
                     </ul>
-                    <div class="search-element">
-                        <input class="form-control" type="search" placeholder="Search" aria-label="Search"
-                            data-width="250" />
-                        <button class="btn" type="submit">
-                            <i class="fas fa-search"></i>
-                        </button>
-                        <div class="search-backdrop"></div>
-                        <div class="search-result">
-                            <div class="search-header">Histories</div>
-                            <div class="search-item">
-                                <a href="#">How to hack NASA using CSS</a>
-                                <a href="#" class="search-close"><i class="fas fa-times"></i></a>
-                            </div>
-                            <div class="search-item">
-                                <a href="#">Kodinger.com</a>
-                                <a href="#" class="search-close"><i class="fas fa-times"></i></a>
-                            </div>
-                            <div class="search-item">
-                                <a href="#">#Stisla</a>
-                                <a href="#" class="search-close"><i class="fas fa-times"></i></a>
-                            </div>
-                            <div class="search-header">Result</div>
-                            <div class="search-item">
-                                <a href="#">
-                                    <img class="mr-3 rounded" width="30" src="assets/img/products/product-3-50.png"
-                                        alt="product" />
-                                    oPhone S9 Limited Edition
-                                </a>
-                            </div>
-                            <div class="search-item">
-                                <a href="#">
-                                    <img class="mr-3 rounded" width="30" src="assets/img/products/product-2-50.png"
-                                        alt="product" />
-                                    Drone X2 New Gen-7
-                                </a>
-                            </div>
-                            <div class="search-item">
-                                <a href="#">
-                                    <img class="mr-3 rounded" width="30"
-                                        src="assets/img/products/product-1-50.png" alt="product" />
-                                    Headphone Blitz
-                                </a>
-                            </div>
-                            <div class="search-header">Projects</div>
-                            <div class="search-item">
-                                <a href="#">
-                                    <div class="search-icon bg-danger text-white mr-3">
-                                        <i class="fas fa-code"></i>
-                                    </div>
-                                    Stisla Admin Template
-                                </a>
-                            </div>
-                            <div class="search-item">
-                                <a href="#">
-                                    <div class="search-icon bg-primary text-white mr-3">
-                                        <i class="fas fa-laptop"></i>
-                                    </div>
-                                    Create a new Homepage Design
-                                </a>
-                            </div>
-                        </div>
-                    </div>
                 </form>
                 <ul class="navbar-nav navbar-right">
-                    <li class="dropdown"><a href="#" data-toggle="dropdown"
+                    <li class="dropdown">
+                        <a href="#" data-toggle="dropdown"
                             class="nav-link dropdown-toggle nav-link-lg nav-link-user">
                             <img alt="image"
-                                src="rc="{{ isset($data_pelanggan) && $data_pelanggan->foto_pelanggan ? url($data_pelanggan->foto_pelanggan) : asset('assets/images/components/avatar.png') }}""
+                                src="{{ Auth::guard('pegawai')->user()->foto_pegawai ?? asset('assets/images/components/avatar.png') }}"
                                 class="rounded-circle mr-1">
-                            <div class="d-sm-none d-lg-inline-block">Hi, {{ Auth::user() }}</div>
+                            <div class="d-sm-none d-lg-inline-block">
+                                Hi, {{ Auth::guard('pegawai')->user()->nama_pegawai ?? 'Guest' }}
+                            </div>
                         </a>
                         <div class="dropdown-menu dropdown-menu-right">
-                            <div class="dropdown-title">Logged in 5 min ago</div>
                             <a href="features-profile.html" class="dropdown-item has-icon">
                                 <i class="far fa-user"></i> Profile
-                            </a>
-                            <a href="features-settings.html" class="dropdown-item has-icon">
-                                <i class="fas fa-cog"></i> Settings
                             </a>
                             <div class="dropdown-divider"></div>
                             <form id="logout-form" action="{{ route('pos.logout') }}" method="POST"
                                 style="display: none;">
                                 @csrf
+                                <input type="hidden" name="id_bengkel" value="{{ $bengkel->id_bengkel }}">
                             </form>
                             <a href="#" class="dropdown-item has-icon text-danger"
                                 onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
@@ -176,31 +130,50 @@
             <div class="main-sidebar sidebar-style-2">
                 <aside id="sidebar-wrapper">
                     <div class="sidebar-brand">
-                        <a href="index.html">Stisla</a>
+                        <a href="{{ route('pos.index', ['id_bengkel' => $bengkel->id_bengkel]) }}">
+                            <img src="  {{ asset('assets/images/logo/logo_side.png') }}" alt=""
+                                style="width: 85%" height="45px">
+                        </a>
                     </div>
                     <div class="sidebar-brand sidebar-brand-sm">
-                        <a href="index.html">St</a>
+                        <a href="{{ route('pos.index', ['id_bengkel' => $bengkel->id_bengkel]) }}">
+                            <img src="{{ asset('assets/images/logo/icon.png') }}" alt="" style="height: 50px"
+                                width="50px">
+                        </a>
                     </div>
                     <ul class="sidebar-menu">
-                        <li class="menu-header">Dashboard</li>
-                        <li>
+                        <li class="menu-header">Main Menu</li>
+                        <li class="{{ request()->routeIs('pos.index') ? 'active' : '' }}">
                             <a class="nav-link"
                                 href="{{ route('pos.index', ['id_bengkel' => $bengkel->id_bengkel]) }}"><i
                                     class="fas fa-home"></i>
                                 <span>Dashboard</span>
                             </a>
                         </li>
-                        <li class="dropdown ">
-                            <a href="" class="nav-link has-dropdown"><i class="fas fa-data"></i><span>Master
-                                    Data</span>
-                            </a>
+                        <li class="dropdown">
+                            <a href="#" class="nav-link has-dropdown"><i
+                                    class="fas fa-database"></i><span>Master
+                                    Data</span></a>
                             <ul class="dropdown-menu">
-                                <li>
+                                <li class="{{ request()->routeIs('pos.menu.index') ? 'active' : '' }}">
                                     <a class="nav-link"
                                         href="{{ route('pos.menu.index', ['id_bengkel' => $bengkel->id_bengkel]) }}">Menu</a>
                                 </li>
                                 <li>
                                     <a class="nav-link" href="index.html">Ecommerce Dashboard</a>
+                                </li>
+                            </ul>
+                        </li>
+                        <li class="dropdown">
+                            <a href="#" class="nav-link has-dropdown"><i class="fas fa-ellipsis-h"></i>
+                                <span>Utilities</span></a>
+                            <ul class="dropdown-menu">
+                                <li><a href="utilities-contact.html">Contact</a></li>
+                                <li>
+                                    <a class="nav-link" href="utilities-invoice.html">Invoice</a>
+                                </li>
+                                <li>
+                                    <a href="utilities-subscribe.html">Subscribe</a>
                                 </li>
                             </ul>
                         </li>
@@ -211,6 +184,9 @@
             <!-- Main Content -->
             <div class="main-content">
                 <section class="section">
+                    <div class="section-header">
+                        <h1>{{ $header }}</h1>
+                    </div>
                     @yield('content')
                 </section>
             </div>
@@ -223,20 +199,28 @@
         </div>
     </div>
 
-    {{-- Template JS --}}
-
+    {{-- General JS --}}
     <script src="{{ asset('template_pos/modules/jquery.min.js') }}"></script>
+    <script src="{{ asset('template_pos/modules/popper.js') }}"></script>
+    <script src="{{ asset('template_pos/modules/tooltip.js') }}"></script>
     <script src="{{ asset('template_pos/modules/bootstrap/js/bootstrap.min.js') }}"></script>
     <script src="{{ asset('template_pos/modules/nicescroll/jquery.nicescroll.min.js') }}"></script>
-    <script src="{{ asset('template_pos/modules/summernote/summernote-bs4.js') }}"></script>
-    <script src="{{ asset('template_pos/modules/moment.min.js') }}"></script>
+    <script src="{{ asset('template_pos//modules/moment.min.js') }}"></script>
     <script src="{{ asset('template_pos/js/stisla.js') }}"></script>
+
+    {{-- JS libraries --}}
+    <script src="{{ asset('template_pos/modules/simple-weather/jquery.simpleWeather.min.js') }}"></script>
+    <script src="{{ asset('template_pos/modules/chart.min.js') }}"></script>
+    <script src="{{ asset('template_pos/modules/jqvmap/dist/jquery.vmap.min.js') }}"></script>
+    <script src="{{ asset('template_pos/modules/jqvmap/dist/maps/jquery.vmap.world.js') }}"></script>
+    <script src="{{ asset('template_pos/modules/summernote/summernote-bs4.js') }}"></script>
+    <script src="{{ asset('template_pos/modules/chocolat/dist/js/jquery.chocolat.min.js') }}"></script>
+
+    {{-- Template JS file --}}
     <script src="{{ asset('template_pos/js/scripts.js') }}"></script>
     <script src="{{ asset('template_pos/js/custom.js') }}"></script>
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/2.10.2/umd/popper.min.js"></script>
 
-    {{-- Aos --}}
+
     <script src="https://cdnjs.cloudflare.com/ajax/libs/aos/2.3.4/aos.js"></script>
 
     {{-- Toastr JS --}}
@@ -300,6 +284,7 @@
             return rupiah;
         }
     </script>
+
 </body>
 
 </html>
