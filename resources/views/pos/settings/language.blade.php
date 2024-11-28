c5d3b207c0a1490ebef8c252ea24e3ad9a304c8b
RaffDev31 <raffli4000@hotmail.com>
2024-11-28 15:04:34 +0700

    feat: add new page for setting language in pos

---
+++ resources/views/pos/settings/language.blade.php
@@ -0,0 +1,30 @@
+@extends('pos.layouts.app')
+@section('title')
+    eBengkelku | Language Settings
+@stop
+@php
+    $header = 'Language Settings';
+@endphp
+@section('content')
+<section class="section">
+    <div class="row">
+        <div class="col-12">
+            <div class="card shadow">
+                <div class="card-body">
+                    <form action="{{ route('pos.change.language', $bengkel->id_bengkel) }}" method="POST">
+                        @csrf
+                        <div class="form-group">
+                            <label for="languageSelect">Select Language</label>
+                            <select class="form-control select2" id="languageSelect" name="language">
+                                <option value="en" {{ app()->getLocale() == 'en' ? 'selected' : '' }}>English</option>
+                                <option value="id" {{ app()->getLocale() == 'id' ? 'selected' : '' }}>Bahasa Indonesia</option>
+                            </select>
+                        </div>
+                        <button type="submit" class="btn btn-primary">Change Language</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+@endsection
