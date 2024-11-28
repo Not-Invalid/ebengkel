031b5e4e318d35732bb4b324e63948f99e864e81
RaffDev31 <raffli4000@hotmail.com>
2024-11-28 15:03:27 +0700

    feat: enhance change password form with error handling and action route

--- resources/views/pos/settings/changePassword.blade.php
+++ resources/views/pos/settings/changePassword.blade.php
@@ -9,16 +9,16 @@
 <section class="section">
     <div class="row">
         <div class="col-12">
-            <div class="card">
-                <div class="card-header">
-                    <h5 class="card-title">Change Password</h5>
-                </div>
+            <div class="card shadow">
                 <div class="card-body">
-                    <form action="" method="POST">
+                    <form action="{{ route('settings.updatePassword', ['id_bengkel' => $bengkel->id_bengkel]) }}" method="POST">
                         @csrf
                         <div class="form-group my-2">
                             <label for="currentPassword" class="form-label">Current Password</label>
                             <input type="password" name="currentPassword" id="currentPassword" class="form-control" placeholder="Enter your current password">
+                            @error('currentPassword')
+                                <div class="text-danger">{{ $message }}</div>
+                            @enderror
                         </div>
                         <div class="form-group my-2">
                             <label for="newPassword" class="form-label">New Password</label>
@@ -27,6 +27,9 @@
                         <div class="form-group my-2">
                             <label for="confirmPassword" class="form-label">Confirm Password</label>
                             <input type="password" name="newPassword_confirmation" id="confirmPassword" class="form-control" placeholder="Confirm new password">
+                            @error('newPassword')
+                                <div class="text-danger">{{ $message }}</div>
+                            @enderror
                         </div>
                         <div class="form-group my-2 d-flex justify-content-end">
                             <button type="submit" class="btn btn-primary">Save Changes</button>
