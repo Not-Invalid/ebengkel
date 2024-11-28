e7eeb1a3018c5e6622af93c10455107476867d56
RaffDev31 <raffli4000@hotmail.com>
2024-11-28 16:28:06 +0700

    fix: correct spelling of "required" in header

--- resources/views/pos/masterdata-product/edit.blade.php
+++ resources/views/pos/masterdata-product/edit.blade.php
@@ -57,7 +57,7 @@
     <div class="card">
         <div class="card-header">
             <h4 class="text-danger">
-                * Indicated requred fields
+                * Indicated required fields
             </h4>
         </div>
         <div class="card-body">
