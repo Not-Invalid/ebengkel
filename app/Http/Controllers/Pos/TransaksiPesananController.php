5b11bfd95c784cc78f1caab66f6f5c0a3e781992
Fawasdiba <fawasabid@gmail.com>
2024-11-30 16:20:02 +0700

    feat:add adjust in TransaksiPesananController

--- app/Http/Controllers/Pos/TransaksiPesananController.php
+++ app/Http/Controllers/Pos/TransaksiPesananController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Pos;
 
 use App\Http\Controllers\Controller;
 use App\Models\Bengkel;
+use App\Models\PesananService;
 use App\Models\OrderOnline;
 use Illuminate\Http\Request;
 
@@ -15,22 +16,19 @@ class TransaksiPesananController extends Controller
         if (!$bengkel) {
             return redirect()->route('profile.workshop')->with('error_status', 'Bengkel tidak ditemukan.');
         }
-
         $perPage = $request->get('per_page', 10);
 
-        $orders = OrderOnline::with('pelanggan')
-            ->where('id_outlet', $id_bengkel)
-            ->where('is_delete', 'N')
-            ->orderBy('tanggal', 'desc')
-            ->paginate(10); // 10 data per halaman
+        $orders = PesananService::with('pelanggan')
+            ->where('id_bengkel', $id_bengkel)
+            ->orderBy('tgl_pesanan', 'desc')
+            ->paginate($perPage);
 
         $totalEntries = $orders->total();
         $start = ($orders->currentPage() - 1) * $perPage + 1;
         $end = min($orders->currentPage() * $perPage, $totalEntries);
-
-
         return view('pos.master-transaksi.pesanan.index', compact('bengkel', 'orders', 'totalEntries', 'start', 'end', 'id_bengkel'));
     }
+
     public function create($id_bengkel)
     {
         $bengkel = Bengkel::find($id_bengkel);
