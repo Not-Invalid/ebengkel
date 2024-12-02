89d98781775236c7cb677f35d6bf51493b435082
dinoarr <dinoananda.restyanto@gmail.com>
2024-12-02 16:23:22 +0700

    feat: add multi-language functionality

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,17 +3,19 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
-use App\Http\Middleware\SessionTimeout;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->alias([
             'session.timeout' => \App\Http\Middleware\SessionTimeout::class,
+        ]);
+        $middleware->alias([
+            'lang' => \App\Http\Middleware\SetLocale::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
