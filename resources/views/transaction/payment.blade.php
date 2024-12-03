7b7eaea0bcfb1f0915b829d06177c15b46e7d67f
RaffDev31 <raffli4000@hotmail.com>
2024-12-03 20:14:36 +0700

    fix(payment): improve payment token handling and user feedback

--- resources/views/transaction/payment.blade.php
+++ resources/views/transaction/payment.blade.php
@@ -16,24 +16,25 @@
         <script src="https://app.sandbox.midtrans.com/snap/snap.js" data-client-key="{{ config('midtrans.clientKey') }}"></script>
 
         <script type="text/javascript">
-            // Menginisialisasi Snap dengan Snap Token yang diberikan
-            snap.pay("{{ $snapToken }}", {
-                onSuccess: function(result) {
-                    alert("Pembayaran berhasil!");
-                    // Setelah pembayaran berhasil, arahkan pengguna ke halaman Cart
-                    window.location.href = "{{ route('cart.index') }}";
-                },
-                onPending: function(result) {
-                    alert("Pembayaran Anda masih pending.");
-                    // Jika pembayaran masih pending, arahkan pengguna ke halaman Cart
-                    window.location.href = "{{ route('cart.index') }}";
-                },
-                onError: function(result) {
-                    alert("Pembayaran gagal.");
-                    // Jika pembayaran gagal, arahkan pengguna ke halaman Cart
-                    window.location.href = "{{ route('cart.index') }}";
-                }
-            });
+            if("{{ $snapToken }}") {
+                snap.pay("{{ $snapToken }}", {
+                    onSuccess: function(result) {
+                        alert("Pembayaran berhasil!");
+                        window.location.href = "{{ route('cart.index') }}";
+                    },
+                    onPending: function(result) {
+                        alert("Pembayaran Anda masih pending.");
+                        window.location.href = "{{ route('cart.index') }}";
+                    },
+                    onError: function(result) {
+                        alert("Pembayaran gagal.");
+                        window.location.href = "{{ route('cart.index') }}";
+                    }
+                });
+            } else {
+                alert('Token pembayaran tidak tersedia.');
+                window.location.href = "{{ route('cart.index') }}";
+            }
         </script>
 
     @else
