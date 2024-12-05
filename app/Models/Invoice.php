3f6d5101239d827db2fba81b65a219d38555d367
RaffDev31 <raffli4000@hotmail.com>
2024-12-05 21:28:54 +0700

    feat(model): create Invoice model with relationships and fillable attributes

---
+++ app/Models/Invoice.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Invoice extends Model
+{
+    use HasFactory;
+
+    protected $table = 't_invoice';
+
+    protected $fillable = [
+        'id_pelanggan',
+        'id_order',
+        'status_invoice',
+        'jatuh_tempo',
+        'tanggal_invoice',
+        'tanggal_bayar',
+        'nama_rekening',
+        'no_rekening',
+        'note',
+        'tanggal_transfer',
+        'nominal_transfer',
+        'bukti_bayar',
+        'is_delete',
+    ];
+
+    public function order()
+    {
+        return $this->belongsTo(OrderOnline::class, 'id_order', 'order_id');
+    }
+
+
+    public function pelanggan()
+    {
+        return $this->belongsTo(Pelanggan::class, 'id_pelanggan', 'id');
+    }
+}
