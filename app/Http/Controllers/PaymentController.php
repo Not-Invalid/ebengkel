f6f29bfa955d13a25d5887e65ae9fe4b9ca93c4d
RaffDev31 <raffli4000@hotmail.com>
2024-12-05 16:04:54 +0700

    refactor(payment): update payment handling to use order ID

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -2,39 +2,24 @@
 
 namespace App\Http\Controllers;
 
-use App\Services\MidtransService;
+use App\Models\OrderOnline;
 use Illuminate\Http\Request;
 
 class PaymentController extends Controller
 {
-    protected $midtransService;
-
-    public function __construct(MidtransService $midtransService)
+    public function index(Request $request, $order_id)
     {
-        $this->midtransService = $midtransService;
-    }
+        // Mengambil data order berdasarkan order_id
+        $order = OrderOnline::where('order_id', $order_id)->first();
 
-    public function index(Request $request)
-    {
-        $snapToken = $request->get('snap_token');  // Mendapatkan snap_token dari URL query parameter
-
-        if (!$snapToken) {
-            return redirect()->route('cart.index')->with('status_error', 'Invalid payment request.');
+        // Jika order tidak ditemukan, redirect ke halaman lain
+        if (!$order) {
+            return redirect()->route('home')->with('error', 'Order tidak ditemukan.');
         }
 
-        return view('transaction.payment', compact('snapToken'));
-    }
+        $orderItems = $order->id_produk ? $order->produk()->get() : $order->sparepart()->get();
 
-    /**
-     * Handle the Midtrans notification after payment
-     */
-    public function notification(Request $request)
-    {
-        // Panggil service untuk menangani notifikasi dari Midtrans
-        $status = $this->midtransService->handleNotification();
-
-        // Anda bisa melakukan update status order di sini berdasarkan status pembayaran
-        // Misalnya, update status order menjadi 'PAID' jika pembayaran berhasil
-        return response()->json(['status' => $status]);
+        return view('transaction.payment', compact('order', 'orderItems'));
     }
 }
+
