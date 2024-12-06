95cf42a08786f27d50594d2b57957e8baef46207
sidaoaa <muhammaddaffaarsiladzim@gmail.com>
2024-12-06 23:14:33 +0700

    update 5 pict product

---
+++ app/Models/FotoProduk.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class FotoProduk extends Model
+{
+    use HasFactory;
+
+    protected $table = 'tb_foto_produk';
+
+    protected $fillable = [
+        'id_produk',
+        'file_foto_produk_1',
+        'file_foto_produk_2',
+        'file_foto_produk_3',
+        'file_foto_produk_4',
+        'file_foto_produk_5',
+        'create_file_foto_produk',
+        'delete_file_foto_produk',
+    ];
+
+    protected $dates = [
+        'create_file_foto_produk',
+        'created_at',
+        'updated_at',
+    ];
+
+    public function produk()
+    {
+        return $this->belongsTo(Product::class, 'id_produk', 'id_produk');
+    }
+}
