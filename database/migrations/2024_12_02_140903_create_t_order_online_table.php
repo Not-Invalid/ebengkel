dfb347de167a5ce87e1715130b451e9b55aaadf4
RaffDev31 <raffli4000@hotmail.com>
2024-12-07 16:34:57 +0700

    refactor: update t_order_online migration

--- database/migrations/2024_12_02_140903_create_t_order_online_table.php
+++ database/migrations/2024_12_02_140903_create_t_order_online_table.php
@@ -18,12 +18,10 @@ class CreateTOrderOnlineTable extends Migration
             $table->string('order_id', 20)->unique();
             $table->integer('id_bengkel')->nullable();
             $table->integer('id_pelanggan')->nullable();
-            $table->unsignedInteger('id_produk')->nullable();
-            $table->integer('id_spare_part')->nullable();
             $table->dateTime('tanggal')->nullable();
-            $table->integer('total_qty')->default(0);
             $table->decimal('total_harga', 10, 0)->nullable();
-            $table->string('status_pengiriman', 50)->nullable();
+            $table->string('status_order', 50)->default('TEMP');
+            $table->string('jenis_pengiriman', 50)->nullable();
             $table->string('kurir', 50)->nullable();
             $table->string('biaya_pengiriman', 50)->nullable();
             $table->string('grand_total', 50)->nullable();
@@ -32,22 +30,19 @@ class CreateTOrderOnlineTable extends Migration
             $table->string('provinsi', 100)->nullable();
             $table->string('kabupaten', 100)->nullable();
             $table->string('kecamatan', 100)->nullable();
-            $table->string('desa', 100)->nullable();
             $table->string('kode_pos', 50)->nullable();
             $table->string('no_telp', 50)->nullable();
-            $table->string('jenis_pengiriman', 50)->nullable();
+            $table->string('status_pengiriman', 50)->nullable();
             $table->string('no_resi', 50)->nullable();
             $table->dateTime('tanggal_kirim')->nullable();
             $table->dateTime('tanggal_diterima')->nullable();
-            $table->string('status_order', 50)->default('TEMP');
             $table->string('is_delete', 1)->default('N');
+            $table->timestamps();
 
             $table->foreign('id_bengkel')->references('id_bengkel')->on('tb_bengkel')->onDelete('cascade');
             $table->foreign('id_pelanggan')->references('id_pelanggan')->on('tb_pelanggan')->onDelete('cascade');
-            $table->foreign('id_produk')->references('id_produk')->on('tb_produk')->onDelete('cascade');
-            $table->foreign('id_spare_part')->references('id_spare_part')->on('tb_spare_part')->onDelete('cascade');
-
         });
+
     }
 
     /**
