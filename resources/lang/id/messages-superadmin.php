c225e3b5c9964874e81b54b7a6bfbce4ff78e21e
Fawasdiba <fawasabid@gmail.com>
2024-12-17 09:52:58 +0700

    feat:added new en and id language files for superadmin

---
+++ resources/lang/id/messages-superadmin.php
@@ -0,0 +1,260 @@
+<?php
+
+return [
+    'controller_pos' => [
+        'event_controller' => [
+            'create' => 'Acara telah berhasil dibuat.',
+            'update' => 'Acara telah berhasil diperbarui.',
+            'error' => 'Terjadi kesalahan saat memperbarui acara.',
+            'delete' => 'Acara telah berhasil dihapus.',
+        ],
+        'blog_category' => [
+            'create' => 'Kategori Blog telah berhasil dibuat.',
+            'update' => 'Kategori Blog telah berhasil diperbarui.',
+            'delete' => 'Kategori Blog telah berhasil dihapus.'
+        ],
+        'car_brand' => [
+            'create' => 'Merek Mobil telah berhasil dibuat.',
+            'update' => 'Merek Mobil telah berhasil diperbarui.',
+            'delete' => 'Merek Mobil telah berhasil dihapus.'
+        ],
+        'product_sparepart' => [
+            'create' => 'Kategori Produk & Sparepart telah berhasil dibuat.',
+            'update' => 'Kategori Produk & Sparepart telah berhasil diperbarui.',
+            'delete' => 'Kategori Produk & Sparepart telah berhasil dihapus.'
+        ],
+        'profile' => [
+            'update' => 'Profil telah berhasil diperbarui.',
+        ],
+        'setting' => [
+            'error_user' => 'Pengguna tidak ditemukan',
+            'error_pw' => 'Kata sandi saat ini salah.',
+            'succes_pw' => 'Kata sandi berhasil diubah! Silakan masuk lagi.',
+        ],
+        'staff' => [
+            'create' => 'Staf telah berhasil dibuat.',
+            'update' => 'Staf telah berhasil diperbarui.',
+            'delete' => 'Staf telah berhasil dihapus.',
+        ],
+        'blog' => [
+            'create' => 'Blog telah berhasil dibuat.',
+            'update' => 'Blog telah berhasil diperbarui.',
+            'delete' => 'Blog telah berhasil dihapus.',
+        ],
+        'auth' => [
+            'login' => 'Kredensial yang diberikan tidak cocok dengan catatan kami.',
+            'login2' => 'Login berhasil!',
+            'erro_login' => 'Login gagal. Silakan periksa kredensial Anda.',
+            'logout' => 'Keluar berhasil!',
+        ],
+
+        'support_center' => [
+            'create' => 'Kategori Dukungan berhasil ditambahkan.',
+            'update' => 'Kategori Dukungan berhasil diperbarui.',
+            'delete' => 'Kategori Dukungan berhasil dihapus.',
+            'c_create' => 'Informasi Dukungan berhasil ditambahkan.',
+            'c_update' => 'Informasi Dukungan berhasil diperbarui.',
+            'c_delete' => 'Informasi Dukungan berhasil dihapus.',
+        ],
+    ],
+
+    'sidebar' => [
+        'profile' => 'Akun',
+        'event' => 'Acara',
+        'inbox' => 'Kotak Masuk',
+        'support_center' => 'Pusat Dukungan',
+        'category' => 'Kategori',
+        'product_spare_parts' => 'Produk & Spare Part',
+        'brand' => 'Merk',
+        'used_car' => 'Mobil Bekas',
+        'workshop' => 'Bengkel',
+        'management_users' => 'Management Pengguna',
+        'general' => 'Umum',
+        'setting' => 'Pengaturan',
+        'change_password' => 'Ganti Kata Sandi',
+        'language_setting' => 'Pengaturan Bahasa',
+        'customer_data' => 'Data Pelanggan',
+        'admin_staff_data' => 'Data Staff Admin',
+
+        'dashboard' => [
+            'register_workshop' => 'Bengkel Terdaftar',
+            'register_user' => 'User Terdaftar'
+        ],
+        'message' => [
+            'message_list' => 'Daftar Pesan',
+            'sender_name' => 'Nama Pengirim',
+            'phone_number' => 'No Telepon',
+            'message' => 'Pesan',
+            'action' => 'Aksi',
+            'view' => 'Lihat',
+            'full_message' => 'Pesan Lengkap'
+        ],
+
+        'blog' => [
+            'add_article' => 'Tambahkan Article Baru',
+            'add_blog' => 'Tambahkan Blog Baru',
+            'category_blog' => 'Kategori Blog',
+            'edit_category_blog' => 'Edit Kategori Blog',
+            'add_category_blog' => 'Tambah Kategori Blog Baru',
+            'cover_photo' => 'Foto Sampul',
+            'select_category' => 'Pilih Kategori',
+            'title' => 'judul',
+            'title_blog' => 'Judul Blog',
+            'content_blog' => 'Konten Blog',
+            'author' => 'Penulis',
+            'write' => 'Tulis Content Blog Anda',
+            'action' => 'Aksi'
+        ],
+
+        'info_support_center' => [
+            'info' => 'Info Pusat Dukungan',
+            'support_categories' => 'Kategori Pusat Dukungan',
+            'add_info' => 'Tambahkan Info Baru',
+            'add_new' => 'Tambahkan Pusat Dukungan Baru',
+            'add_new_categories' => 'Tambahkan Kategori Baru',
+            'edit_categories' => 'Edit Kategori',
+            'question' => 'Pertanyaan',
+            'answer' => 'Jawaban',
+            'action' => 'Aksi',
+            'icon' => 'Ikon',
+            'icon_preview' => 'Pratinjau Ikon',
+            'categories_name' => 'Nama Kategori',
+            'enter' => 'Masukkan nama kelas Font Awesome',
+            'user' => 'pengguna',
+            'heart' => 'hati',
+            'option' => 'Lihat opsi Ikon di',
+            'aws_web' => 'Website Fontt Awesome',
+        ],
+
+        'info_event' => [
+            'list_event' => 'Daftar Acara',
+            'nama' => 'Nama',
+            'add_event' => 'Tambahkan Acara Baru',
+            'photo_cover' => 'Foto Sampul',
+            'event_name' => 'Nama Acara',
+            'event_start' => 'Tanggal Mulai Acara',
+            'event_end' => 'Tanggal Berakhir Acara',
+            'description' => 'Deskripsi',
+            'event_adress' => 'Alamat Acara',
+            'event_location' => 'Lokasi',
+            'event_price' => 'Pilih Tipe Harga',
+            'event_free' => 'Gratis',
+            'event_paid' => 'Berbayar',
+            'price_type' => 'Tipe Harga',
+            'price' => 'Harga',
+            'agenda_title' => 'Judul Agenda',
+            'no_agenda' => 'Tidak ada Agenda yang Tersedia.',
+            'no_star' => 'Tidak ada Bintang Tamu yang Tersedia.',
+            'time' => 'Waktu',
+            'add_agenda' => 'Tambah Agenda',
+            'guest_star_name' => 'Nama Bintang Tamu',
+            'guest_star' => 'Bintang Tamu',
+            'add_guest' => 'Tambah Bintang Tamu',
+            'add_participant' => 'Daftar Peserta Event',
+            'payment_status' => 'Status Pembayaran',
+            'phone_number' => 'No Telepon',
+        ],
+
+        'info_profile' => [
+            'profile' => 'Profil Akun',
+            'profile_picture' => 'Tinjauan Foto Profil',
+            'name' => 'nama',
+            'profil_phone' => 'Telepon',
+            'profil_photo' => 'Foto Profil',
+            'profil_edit' => 'Ubah Profil',
+        ],
+
+        'info_category' => [
+            'psc' => 'Kategori Produk & SparePart',
+            'add_category' => 'Tambah Kategori Baru',
+            'edit_category' => 'Edit Kategori',
+            'category_name' => 'Nama Kategori',
+            'action' => 'Aksi',
+        ],
+
+        'info_merk' => [
+            'list_merk' => 'Daftar Brand Mobil',
+            'add_merk' => 'Tambah Brand Baru',
+            'edit_merk' => 'Edit Brand',
+            'name_merk' => 'Nama Brand',
+            'action' => 'Aksi',
+        ],
+
+        'info_workshop' => [
+            'regis_workshop' => 'Daftar Workshop Terdaftar',
+            'name_workshop' => 'Nama Bengkel',
+            'open_workshop' => 'Buka Bengkel',
+            'owner' => 'Pemilik',
+            'action' => 'Aksi',
+            'location' => 'Lokasi',
+            'count_product' => 'Total Produk',
+            'count_sparepart' => 'Total SparePart',
+            'count_service' => 'Total Service',
+            'rating' => 'Rating',
+            'all' => 'Semua',
+            'product' => 'Produk',
+            'sparepart' => 'Sparepart',
+            'service' => 'Service',
+            'sold_product' => 'Produk Terjual',
+            'sold_service' => 'Service Terjual',
+        ],
+
+        'info_data_pelanggan' => [
+            'list_pelanggan' => 'Daftar Data Pelanggan',
+            'name_pelanggan' => 'Nama Pelanggan',
+            'phone_pelanggan' => 'Telepon Pelanggan',
+            'email_pelanggan' => 'Email Pelanggan',
+            'peran' => 'Peran',
+            'delete_pelanggan' => 'Hapus Pelanggan',
+            'aksi' => 'Aksi',
+        ],
+
+        'staff_admin' => [
+            'data_admin' => 'Data Staf Admin',
+            'add_staff' => 'Tambah Staf Baru',
+            'name' => 'Nama Staf',
+            'phone' => 'Nomor Telepon',
+            'role' => 'Peran',
+            'action' => 'Tindakan',
+            'select' => 'Tentukan Role',
+            'user' => 'Pengguna',
+            'password' => 'Kata Sandi',
+            'confirm_password' => 'Konfirmasi Kata Sandi',
+            'edit_admin' => 'Edit Staf Admin',
+        ],
+
+        'password' => [
+            'change' => 'Ubah Kata Sandi',
+            'current' => 'Kata Sandi saat ini',
+            'new' => 'Kata Sandi Baru',
+            'confirm' => 'Konfirmasi Kata Sandi',
+            'save' => 'Simpan Perubahan',
+            'placeholder' => [
+                'current' => 'Masukkan kata sandi Anda saat ini',
+                'new' => 'Masukkan kata sandi baru',
+                'confirm' => 'Konfirmasi kata sandi baru',
+            ],
+        ],
+
+        'month' => [
+            'january' => 'Januari',
+            'february' => 'Februari',
+            'march' => 'Maret',
+            'april' => 'April',
+            'may' => 'Mei',
+            'june' => 'Juni',
+            'july' => 'Juli',
+            'august' => 'Agustus',
+            'september' => 'September',
+            'october' => 'Oktober',
+            'november' => 'November',
+            'december' => 'Desember',
+        ],
+
+        'button' => [
+            'cancel' => 'Batal',
+            'save' => 'Simpan'
+        ],
+        'not_found' => 'Data tidak ditemukan.'
+    ],
+];
