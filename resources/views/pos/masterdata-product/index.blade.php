ca08ee3d2a0822c00a5e5878d98226ee4b61e2ef
sidaoaa <muhammaddaffaarsiladzim@gmail.com>
2024-12-20 13:22:57 +0700

    update empty product

--- resources/views/pos/masterdata-product/index.blade.php
+++ resources/views/pos/masterdata-product/index.blade.php
@@ -14,45 +14,51 @@
     <div class="col">
       <div class="card shadow-sm">
         <div class="card-body">
-          <div class="d-flex justify-between">
-            <div class="d-flex justify-start mb-4">
-              <select name="per_page" id="perPage" class="form-control w-auto mx-2">
-                <option value="10" {{ request('per_page') == '10' ? 'selected' : '' }}>10</option>
-                <option value="25" {{ request('per_page') == '25' ? 'selected' : '' }}>25</option>
-                <option value="50" {{ request('per_page') == '50' ? 'selected' : '' }}>50</option>
-                <option value="100" {{ request('per_page') == '100' ? 'selected' : '' }}>100</option>
-              </select>
+          @if ($products->isEmpty())
+            <div class="empty-state" data-height="400" style="height: 400px;">
+              <div class="empty-state-icon">
+                <i class="fas fa-question"></i>
+              </div>
+              <h2>We couldn't find any data</h2>
+              <a href="{{ route('pos.product.create', $bengkel->id_bengkel) }}" class="btn btn-primary mt-4">Create new
+                One</a>
+            </div>
+          @else
+            <div class="d-flex justify-between">
+              <div class="d-flex justify-start mb-4">
+                <select name="per_page" id="perPage" class="form-control w-auto mx-2">
+                  <option value="10" {{ request('per_page') == '10' ? 'selected' : '' }}>10</option>
+                  <option value="25" {{ request('per_page') == '25' ? 'selected' : '' }}>25</option>
+                  <option value="50" {{ request('per_page') == '50' ? 'selected' : '' }}>50</option>
+                  <option value="100" {{ request('per_page') == '100' ? 'selected' : '' }}>100</option>
+                </select>
+              </div>
+
+              <div class="d-flex justify-center w-100 mx-2 mb-4">
+                <input type="text" id="search" class="form-control w-60" placeholder="Search">
+              </div>
+
+              <div class="d-flex justify-end mb-4">
+                <a href="{{ route('pos.product.create', ['id_bengkel' => $bengkel->id_bengkel]) }}"
+                  class="btn btn-info text-white px-4 py-2 mx-2">Add Product</a>
+              </div>
             </div>
 
-            <div class="d-flex justify-center w-100 mx-2 mb-4">
-              <input type="text" id="search" class="form-control w-60" placeholder="Search">
-            </div>
-
-            <div class="d-flex justify-end mb-4">
-              <a href="{{ route('pos.product.create', ['id_bengkel' => $bengkel->id_bengkel]) }}"
-                class="btn btn-info text-white px-4 py-2 mx-2">Add Product</a>
-            </div>
-          </div>
-
-          <div class="table-responsive bg-white rounded shadow-sm">
-            <table class="table table-bordered table-striped text-center">
-              <thead class="bg-light-grey text-white">
-                <tr>
-                  <th class="text-center">No</th>
-                  <th class="text-center">Product Name</th>
-                  <th class="text-center">Product Merk</th>
-                  <th class="text-center">Product Photo</th>
-                  <th class="text-center">Product Stock</th>
-                  <th class="text-center">Product Price</th>
-                  <th class="text-center">Tools</th>
-                </tr>
-              </thead>
-              <tbody id="product-table-body">
-                @if ($products->isEmpty())
+            <div class="table-responsive bg-white rounded shadow-sm">
+              <table class="table table-bordered table-striped text-center">
+                <thead class="bg-light-grey text-white">
                   <tr>
-                    <td colspan="7" class="text-center">Data Not Found</td>
+                    <th class="text-center">No</th>
+                    <th class="text-center">Product Name</th>
+                    <th class="text-center">Product Merk</th>
+                    <th class="text-center">Product Photo</th>
+                    <th class="text-center">Product Stock</th>
+                    <th class="text-center">Product Price</th>
+                    <th class="text-center">Tools</th>
                   </tr>
-                @else
+                </thead>
+                <tbody id="product-table-body">
+
                   @foreach ($products as $index => $product)
                     <tr>
                       <td>{{ $loop->iteration }}</td>
@@ -87,51 +93,53 @@
                       </td>
                     </tr>
                   @endforeach
-                @endif
-              </tbody>
-            </table>
-          </div>
 
-          <div class="d-flex justify-content-between mt-4">
-            <div>
-              <span>Showing {{ $start }} to {{ $end }} of {{ $totalEntries }} entries</span>
+                </tbody>
+              </table>
             </div>
-            <div class="d-flex">
-              <nav aria-label="Page navigation">
-                <ul class="pagination">
-                  @if ($products->onFirstPage())
-                    <li class="page-item disabled">
-                      <span class="page-link"><i class="fa-solid fa-chevron-left"></i></span>
-                    </li>
-                  @else
-                    <li class="page-item">
-                      <a href="{{ $products->previousPageUrl() }}" class="page-link"><i
-                          class="fa-solid fa-chevron-left"></i></a>
-                    </li>
-                  @endif
 
-                  @foreach ($products->getUrlRange(1, $products->lastPage()) as $page => $url)
-                    @if ($page == $products->currentPage())
-                      <li class="page-item active"><span class="page-link">{{ $page }}</span></li>
+            <div class="d-flex justify-content-between mt-4">
+              <div>
+                <span>Showing {{ $start }} to {{ $end }} of {{ $totalEntries }} entries</span>
+              </div>
+              <div class="d-flex">
+                <nav aria-label="Page navigation">
+                  <ul class="pagination">
+                    @if ($products->onFirstPage())
+                      <li class="page-item disabled">
+                        <span class="page-link"><i class="fa-solid fa-chevron-left"></i></span>
+                      </li>
                     @else
-                      <li class="page-item"><a href="{{ $url }}" class="page-link">{{ $page }}</a></li>
+                      <li class="page-item">
+                        <a href="{{ $products->previousPageUrl() }}" class="page-link"><i
+                            class="fa-solid fa-chevron-left"></i></a>
+                      </li>
                     @endif
-                  @endforeach
 
-                  @if ($products->hasMorePages())
-                    <li class="page-item">
-                      <a href="{{ $products->nextPageUrl() }}" class="page-link"><i
-                          class="fa-solid fa-chevron-right"></i></a>
-                    </li>
-                  @else
-                    <li class="page-item disabled">
-                      <span class="page-link"><i class="fa-solid fa-chevron-right"></i></span>
-                    </li>
-                  @endif
-                </ul>
-              </nav>
+                    @foreach ($products->getUrlRange(1, $products->lastPage()) as $page => $url)
+                      @if ($page == $products->currentPage())
+                        <li class="page-item active"><span class="page-link">{{ $page }}</span></li>
+                      @else
+                        <li class="page-item"><a href="{{ $url }}" class="page-link">{{ $page }}</a>
+                        </li>
+                      @endif
+                    @endforeach
+
+                    @if ($products->hasMorePages())
+                      <li class="page-item">
+                        <a href="{{ $products->nextPageUrl() }}" class="page-link"><i
+                            class="fa-solid fa-chevron-right"></i></a>
+                      </li>
+                    @else
+                      <li class="page-item disabled">
+                        <span class="page-link"><i class="fa-solid fa-chevron-right"></i></span>
+                      </li>
+                    @endif
+                  </ul>
+                </nav>
+              </div>
             </div>
-          </div>
+          @endif
         </div>
       </div>
     </div>
