0ded30c4ea39d30dd8ee57ead8b865401c468ace
RaffDev31 <raffli4000@hotmail.com>
2024-12-20 09:39:22 +0700

    feat: enhance order editing form with validation and image preview modal

--- resources/views/pos/order/order-online/edit.blade.php
+++ resources/views/pos/order/order-online/edit.blade.php
@@ -8,39 +8,63 @@
     $header = 'Edit Order and Invoice';
 @endphp
 
+@push('css')
+    <style>
+        .form-control:disabled:focus, .form-control[readonly]:focus {
+            background-color: #e9ecef;
+            opacity: 1;
+        },
+    </style>
+@endpush
+
 @section('content')
     <div class="container">
-        <form action="{{ route('pos.order-online.update', ['id_bengkel' => $bengkel->id_bengkel, 'order_id' => $order->order_id]) }}" method="POST">
+        <form
+            action="{{ route('pos.order-online.update', ['id_bengkel' => $bengkel->id_bengkel, 'order_id' => $order->order_id]) }}"
+            id="orderForm" method="POST">
             @csrf
             <div class="form-group">
                 <label for="status_invoice">Status Invoice</label>
                 <select name="status_invoice" id="status_invoice" class="form-control" required>
-                    <option value="PENDING" {{ $order->invoice->status_invoice == 'PENDING' ? 'selected' : '' }}>Pending</option>
-                    <option value="Waiting_Confirmation" {{ $order->invoice->status_invoice == 'Waiting_Confirmation' ? 'selected' : '' }}>Waiting Confirmation</option>
-                    <option value="PAYMENT_CONFIRMED" {{ $order->invoice->status_invoice == 'PAYMENT_CONFIRMED' ? 'selected' : '' }}>Payment Confirmed</option>
+                    <option value="PENDING" {{ $order->invoice->status_invoice == 'PENDING' ? 'selected' : '' }}>Pending
+                    </option>
+                    <option value="Waiting_Confirmation"
+                        {{ $order->invoice->status_invoice == 'Waiting_Confirmation' ? 'selected' : '' }}>Waiting
+                        Confirmation</option>
+                    <option value="PAYMENT_CONFIRMED"
+                        {{ $order->invoice->status_invoice == 'PAYMENT_CONFIRMED' ? 'selected' : '' }}>Payment Confirmed
+                    </option>
                 </select>
             </div>
             <div class="form-group">
                 <label for="status_order">Status Order</label>
                 <select name="status_order" id="status_order" class="form-control" required>
                     <option value="PENDING" {{ $order->status_order == 'PENDING' ? 'selected' : '' }}>Pending</option>
-                    <option value="Waiting_Confirmation" {{ $order->status_order == 'Waiting_Confirmation' ? 'selected' : '' }}>Waiting Confirmation</option>
+                    <option value="Waiting_Confirmation"
+                        {{ $order->status_order == 'Waiting_Confirmation' ? 'selected' : '' }}>Waiting Confirmation</option>
                     <option value="DIKEMAS" {{ $order->status_order == 'DIKEMAS' ? 'selected' : '' }}>Dikemas</option>
                     <option value="DIKIRIM" {{ $order->status_order == 'DIKIRIM' ? 'selected' : '' }}>Dikirim</option>
                     <option value="SELESAI" {{ $order->status_order == 'SELESAI' ? 'selected' : '' }}>Selesai</option>
                 </select>
             </div>
 
+            <div class="form-group">
+                <label for="nomor_resi">Nomor Resi <span class="text-danger">*</span></label>
+                <input type="text" id="nomor_resi" name="nomor_resi" class="form-control" value="{{ old('nomor_resi', $order->no_resi) }}"
+                @if($order->no_resi) readonly @endif>
+                <p class="error-text text-danger mt-1" id="nomorResiError" style="display: none;"></p>
+            </div>
 
             <!-- Info Pelanggan -->
             <div class="form-group">
                 <label for="pelanggan">Nama Pelanggan</label>
-                <input type="text" id="pelanggan" class="form-control" value="{{ $order->pelanggan->nama_pelanggan }}" disabled>
+                <input type="text" id="pelanggan" class="form-control" value="{{ $order->pelanggan->nama_pelanggan }}"
+                    readonly>
             </div>
 
             <div class="form-group">
                 <label for="alamat_pengiriman">Alamat Pengiriman</label>
-                <textarea id="alamat_pengiriman" class="form-control" style="resize: none; height: 100px !important;" disabled>{{ $order->alamat_pengiriman }}</textarea>
+                <textarea id="alamat_pengiriman" class="form-control" style="resize: none; height: 100px !important;" readonly>{{ $order->alamat_pengiriman }}</textarea>
             </div>
 
             <!-- Info Item Order -->
@@ -59,7 +83,7 @@
                         @foreach ($order->orderItems as $item)
                             <tr>
                                 <td>
-                                    @if($item->id_produk)
+                                    @if ($item->id_produk)
                                         {{ $item->produk->nama_produk }}
                                     @elseif($item->id_spare_part)
                                         {{ $item->sparepart->nama_spare_part }}
@@ -79,52 +103,205 @@
             <!-- Info Total Harga -->
             <div class="form-group">
                 <label for="total_harga">Total Harga</label>
-                <input type="text" id="total_harga" class="form-control" value="{{ formatRupiah($order->total_harga) }}" disabled>
+                <input type="text" id="total_harga" class="form-control" value="{{ formatRupiah($order->total_harga) }}"
+                    readonly>
             </div>
 
             <div class="form-group">
                 <label for="total_harga">Grand Total</label>
-                <input type="text" id="grand_total" class="form-control" value="{{ formatRupiah($order->grand_total) }}" disabled>
+                <input type="text" id="grand_total" class="form-control" value="{{ formatRupiah($order->grand_total) }}"
+                    readonly>
             </div>
 
             <!-- Info Invoice -->
             <div class="form-group">
                 <label for="jatuh_tempo">Jatuh Tempo</label>
-                <input type="text" id="jatuh_tempo" class="form-control" value="{{ $order->invoice->jatuh_tempo }}" disabled>
+                <input type="text" id="jatuh_tempo" class="form-control" value="{{ $order->invoice->jatuh_tempo }}"
+                    readonly>
             </div>
 
             <div class="form-group">
                 <label for="tanggal_invoice">Tanggal Invoice</label>
-                <input type="text" id="tanggal_invoice" class="form-control" value="{{ $order->invoice->tanggal_invoice }}" disabled>
+                <input type="text" id="tanggal_invoice" class="form-control"
+                    value="{{ $order->invoice->tanggal_invoice }}" readonly>
+            </div>
+
+            <div class="form-group">
+                <label for="bukti_bayar">Bukti Pembayaran</label>
+                <div class="preview-container d-flex justify-content-center">
+                    <img src="{{ old('bukti_bayar', isset($order->invoice->bukti_bayar) ? asset($order->invoice->bukti_bayar) : asset('assets/images/components/image.png')) }}"
+                        alt="Bukti Bayar Preview" class="image-preview"
+                        style="display: block; margin-top: 10px; cursor: pointer;" id="imagePreview">
+                </div>
             </div>
 
             <div class="d-flex gap-2 justify-content-end">
                 <a href="{{ route('pos.order-online', ['id_bengkel' => $bengkel->id_bengkel]) }}" id="backButton"
-                  class="btn btn-danger">Back</a>
+                    class="btn btn-danger">Back</a>
                 <button type="submit" class="btn btn-primary">Save</button>
             </div>
         </form>
     </div>
 
+    <div id="fire-modal-1" class="modal fade" tabindex="-1" role="dialog" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title">Bukti Pembayaran</h5>
+                    <button type="button" class="close" id="closeModal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <img id="modalImage" src="" alt="Bukti Pembayaran" class="img-fluid">
+                </div>
+            </div>
+        </div>
+    </div>
+
     <script>
-        document.addEventListener('DOMContentLoaded', function () {
-            const statusInvoice = document.getElementById('status_invoice');
-            const statusOrder = document.getElementById('status_order');
-            const form = document.querySelector('form');
+        document.addEventListener('DOMContentLoaded', function() {
+            const statusOrderInput = document.getElementById('status_order');
+            const statusOrderCurrent = "{{ $order->status_order }}"; // Ambil status order saat ini
+            const statusOrderSequence = ['PENDING', 'Waiting_Confirmation', 'DIKEMAS', 'DIKIRIM', 'SELESAI']; // Urutan status yang valid
 
-            statusOrder.addEventListener('change', function () {
-                if (statusInvoice.value !== 'PAYMENT_CONFIRMED') {
-                    alert('You can only edit the order status if the invoice status is PAYMENT_CONFIRMED.');
-                    statusOrder.value = "{{ $order->status_order }}"; // Reset to the original value
-                }
-            });
+            // Fungsi untuk memeriksa apakah transisi status valid
+            function isValidStatusTransition(currentStatus, newStatus) {
+                const currentIndex = statusOrderSequence.indexOf(currentStatus);
+                const newIndex = statusOrderSequence.indexOf(newStatus);
+                return newIndex === currentIndex + 1; // Pastikan status baru adalah status berikutnya dalam urutan
+            }
 
-            form.addEventListener('submit', function (e) {
-                if (statusInvoice.value !== 'PAYMENT_CONFIRMED') {
-                    e.preventDefault();
-                    alert('Cannot submit: The invoice status must be PAYMENT_CONFIRMED to edit the order status.');
+            // Event listener saat status order berubah
+            statusOrderInput.addEventListener('change', function() {
+                const newStatus = statusOrderInput.value;
+
+                // Cek apakah transisi status valid
+                if (!isValidStatusTransition(statusOrderCurrent, newStatus)) {
+                    // Tampilkan pesan error menggunakan Toastr jika status tidak valid
+                    toastr.error('Transisi status tidak valid. Ikuti urutan status yang benar.');
+
+                    // Kembalikan status ke yang sebelumnya
+                    statusOrderInput.value = statusOrderCurrent;
                 }
             });
         });
     </script>
+
+
+    <script>
+        document.addEventListener('DOMContentLoaded', function() {
+            const form = document.getElementById('orderForm');
+            const statusOrderInput = document.getElementById('status_order');
+            const nomorResiInput = document.getElementById('nomor_resi');
+            const nomorResiError = document.getElementById('nomorResiError');
+
+            const statusInvoice = document.getElementById('status_invoice');
+
+            if (!form || !statusOrderInput || !nomorResiInput || !nomorResiError) {
+                console.error("Form atau elemen yang diperlukan tidak ditemukan.");
+                return;
+            }
+
+            function showError(message) {
+                nomorResiError.style.display = 'block';
+                nomorResiError.textContent = message;
+            }
+
+            function hideError() {
+                nomorResiError.style.display = 'none';
+            }
+
+
+            form.addEventListener('submit', function(e) {
+                const statusOrderValue = statusOrderInput.value;
+
+                if (statusOrderValue === 'DIKIRIM' && !nomorResiInput.value.trim()) {
+                    e.preventDefault();
+                    showError('Nomor Resi wajib diinput ketika status order adalah DIKIRIM.');
+
+                    nomorResiInput.focus();
+                    nomorResiInput.scrollIntoView({
+                        behavior: 'smooth',
+                        block: 'center'
+                    });
+
+                } else {
+                    hideError();
+                }
+            });
+
+            statusOrderInput.addEventListener('change', function() {
+                if (statusOrderInput.value === 'DIKIRIM' && !nomorResiInput.value.trim()) {
+                    showError('Nomor Resi wajib diisi ketika status order adalah "DIKIRIM". Pastikan tidak ada kesalahan saat menginput Nomor Resi.');
+                } else {
+                    hideError();
+                }
+
+                if (statusOrderInput.value !== 'DIKIRIM') {
+                    nomorResiInput.disabled = true;
+                } else {
+                    nomorResiInput.disabled = false;
+                }
+            });
+
+            if (statusOrderInput.value !== 'DIKIRIM') {
+                nomorResiInput.disabled = true;
+            } else {
+                nomorResiInput.disabled = false;
+            }
+
+            statusInvoice.addEventListener('change', function() {
+                if (statusInvoice.value !== 'PAYMENT_CONFIRMED') {
+                    statusOrderInput.disabled = true;
+                } else {
+                    statusOrderInput.disabled = false;
+                }
+            });
+
+            if (statusInvoice.value !== 'PAYMENT_CONFIRMED') {
+                statusOrderInput.disabled = true;
+            } else {
+                statusOrderInput.disabled = false;
+            }
+        });
+    </script>
+
+    <script>
+        const imagePreview = document.getElementById('imagePreview');
+        const modal = document.getElementById('fire-modal-1');
+        const modalImage = document.getElementById('modalImage');
+        const closeModal = document.getElementById('closeModal');
+        const closeModalFooter = document.getElementById('closeModalFooter');
+
+        function openModal(imageSrc) {
+            modal.style.display = 'block';
+            modal.classList.add('show');
+            modal.setAttribute('aria-hidden', 'false');
+            modalImage.src = imageSrc;
+        }
+
+        function closeModalFunc() {
+            modal.classList.remove('show');
+            modal.setAttribute('aria-hidden', 'true');
+            setTimeout(() => {
+                modal.style.display = 'none';
+            }, 300);
+        }
+
+        imagePreview.addEventListener('click', function() {
+            const imageSrc = this.src;
+            openModal(imageSrc);
+        });
+
+        closeModal.addEventListener('click', closeModalFunc);
+        closeModalFooter.addEventListener('click', closeModalFunc);
+
+        window.addEventListener('click', function(e) {
+            if (e.target === modal) {
+                closeModalFunc();
+            }
+        });
+    </script>
+
 @endsection
