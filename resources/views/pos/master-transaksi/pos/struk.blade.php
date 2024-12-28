cd27035960f8892e80c0486c050fe0d17cdc5ca7
dinoarr <dinoananda.restyanto@gmail.com>
2024-12-28 14:47:35 +0700

    change title struk

--- resources/views/pos/master-transaksi/pos/struk.blade.php
+++ resources/views/pos/master-transaksi/pos/struk.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no">
-    <title>Struk Pembelian &mdash; Stisla</title>
+    <title>Struk Pembelian</title>
 
     <!-- General CSS Files -->
     <link rel="stylesheet" href="{{ asset('template_pos/modules/bootstrap/css/bootstrap.min.css') }}">
